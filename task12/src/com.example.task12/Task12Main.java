23a2d1cabe596be79cf01b7ce8dbcc8093e0c93b
Dmitry <hacker.dmitry@gmail.com>
2019-02-22 04:37:46 +0500

    Solving task12

--- task12/src/com.example.task12/Task12Main.java
+++ task12/src/com.example.task12/Task12Main.java
@@ -1,18 +1,28 @@
 package com.example.task12;
 
+import java.util.Arrays;
+
 public class Task12Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        int[] arr = {9, 11, 7, 8};
-        selectionSort(arr);
-        System.out.println(java.util.Arrays.toString(arr));
-         */
     }
 
     static void selectionSort(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (Arrays.equals(arr, null) || arr.length == 0) return;
+
+        for (int i = 0, minIndex = i; i < arr.length - 1; i++, minIndex = i) {
+            for (int j = i + 1, min = arr[i]; j < arr.length; j++) {
+                if (arr[j] < min) {
+                    min = arr[j];
+                    minIndex = j;
+                }
+            }
+            swapByIndex(arr, i, minIndex);
+        }
     }
 
+    private static void swapByIndex(int[] arr, int indexA, int indexB) {
+        int t = arr[indexA];
+        arr[indexA] = arr[indexB];
+        arr[indexB] = t;
+    }
 }
