667a0220f397595ebc0ac4e9a7d822cd9a87be9d
Henkka <henkkawar@gmail.com>
2019-02-22 22:43:02 +0500

    task 4

--- task04/src/com.example.task04/Task04Main.java
+++ task04/src/com.example.task04/Task04Main.java
@@ -1,5 +1,7 @@
 package com.example.task04;
 
+import java.util.stream.IntStream;
+
 public class Task04Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
@@ -10,7 +12,8 @@ public class Task04Main {
     }
 
     static void from0to10000() {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        IntStream.range(0, 10000)
+                .forEach(System.out::println);
     }
 
 }
