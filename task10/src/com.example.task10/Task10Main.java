027cdb6ea181b1d275ba6ec194167b48f6afcd78
Dmitriy <Mystache@yandex.ru>
2019-02-22 19:05:24 +0500

    Solution #10

--- task10/src/com.example.task10/Task10Main.java
+++ task10/src/com.example.task10/Task10Main.java
@@ -1,18 +1,19 @@
 package com.example.task10;
+import java.util.Arrays;
 
 public class Task10Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        int[] arr = {7, 5, 9};
+        int[] arr = { 7, 5, 9 };
         System.out.println(numMin(arr));
-         */
     }
 
     static int numMin(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        int[] values = { 0, Integer.MAX_VALUE };
+        for (int i = 0; i < arr.length; i++)
+            if (arr[i] <= values[1]) {
+                values[1] = arr[i];
+                values[0] = i;
+            }
+        return values[0];
     }
-
 }
