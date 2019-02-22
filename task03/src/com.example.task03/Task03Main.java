5bd79c4eed8fdc65da03db3a9d65fa06aa9e935e
Dmitriy <Mystache@yandex.ru>
2019-02-22 17:44:02 +0500

    Solution #3

--- task03/src/com.example.task03/Task03Main.java
+++ task03/src/com.example.task03/Task03Main.java
@@ -2,14 +2,11 @@ package com.example.task03;
 
 public class Task03Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         System.out.println(getNameOfWeekDays(7));
-         */
     }
 
     static String getNameOfWeekDays(int weekDaysNumber) {
-        return "";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        String[] daysOfWeek = { "понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье" };
+        return daysOfWeek[weekDaysNumber - 1].isEmpty() ? "Такого дня недели не существует." : daysOfWeek[weekDaysNumber - 1];
     }
 }
