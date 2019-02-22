bde61ae1fbb28a0784a4ad50b405f49710901228
Dmitriy <Mystache@yandex.ru>
2019-02-22 19:53:33 +0500

    Solution #12

--- task12/src/com.example.task12/Task12Main.java
+++ task12/src/com.example.task12/Task12Main.java
@@ -1,18 +1,35 @@
 package com.example.task12;
+import java.util.Arrays;
 
 public class Task12Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         int[] arr = {9, 11, 7, 8};
         selectionSort(arr);
         System.out.println(java.util.Arrays.toString(arr));
-         */
+    }
+
+    static int getMinIndex(int[] array, int iteration) {
+        int index = 0;
+        int min = Integer.MAX_VALUE;
+        for (int i = iteration; i < array.length; i++)
+            if (array[i] <= min) {
+                min = array[i];
+                index = i;
+            }
+        return index;
+    }
+
+    static void swap(int[] arr, int index, int i) {
+        int temp = arr[index];
+        arr[index] = arr[i];
+        arr[i] = temp;
     }
 
     static void selectionSort(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (Arrays.equals(arr, null) || arr.length == 0) return;
+        for (int i = 0; i < arr.length; i++) {
+            int minIndex = getMinIndex(arr, i);
+            swap(arr, minIndex, i);
+        }
     }
-
 }
