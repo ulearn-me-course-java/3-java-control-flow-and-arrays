ee6dab0053bb9a2fa71b786588895140a941c376
Dmitriy <Mystache@yandex.ru>
2019-02-22 19:21:25 +0500

    Solution #11

--- task11/src/com.example.task11/Task11Main.java
+++ task11/src/com.example.task11/Task11Main.java
@@ -1,18 +1,22 @@
 package com.example.task11;
+import java.util.Arrays;
 
 public class Task11Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        int[] arr = {7, 5, 9};
+        int[] arr = { 7, 5, 9 };
         swap(arr);
         System.out.println(java.util.Arrays.toString(arr));
-         */
     }
 
     static void swap(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (Arrays.equals(arr, null) || arr.length == 0) return;
+        int[] values = {0, Integer.MAX_VALUE, arr[0]};
+        for (int i = 0; i < arr.length; i++)
+            if (arr[i] <= values[1]) {
+                values[1] = arr[i];
+                values[0] = i;
+            }
+        arr[0] = values[1];
+        arr[values[0]] = values[2];
     }
-
 }
