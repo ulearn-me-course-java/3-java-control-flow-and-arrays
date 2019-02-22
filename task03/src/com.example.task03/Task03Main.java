5b3c2c8215c4fe8a465b9c7771578db9cc64ae59
Henkka <henkkawar@gmail.com>
2019-02-22 22:37:45 +0500

    task 3

--- task03/src/com.example.task03/Task03Main.java
+++ task03/src/com.example.task03/Task03Main.java
@@ -1,15 +1,35 @@
 package com.example.task03;
 
+import java.time.DayOfWeek;
+
 public class Task03Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        System.out.println(getNameOfWeekDays(7));
-         */
+
+        //System.out.println(DayOfWeek..of(1).toString());
+
     }
 
     static String getNameOfWeekDays(int weekDaysNumber) {
-        return "";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        switch (weekDaysNumber) {
+            case 1:
+                return "понедельник";
+            case 2:
+                return "вторник";
+            case 3:
+                return "среда";
+            case 4:
+                return "четверг";
+            case 5:
+                return "пятница";
+            case 6:
+                return "суббота";
+            case 7:
+                return "воскресенье";
+            default:
+                return "такого дня недели не существует";
+
+        }
     }
 }
