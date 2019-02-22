8b1d2feaa768f49c4afbece094a0a9c7434ff482
Henkka <henkkawar@gmail.com>
2019-02-22 23:06:14 +0500

    task 5

--- task05/src/com.example.task05/Task05Main.java
+++ task05/src/com.example.task05/Task05Main.java
@@ -1,5 +1,7 @@
 package com.example.task05;
 
+import java.util.stream.IntStream;
+
 public class Task05Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
@@ -10,7 +12,11 @@ public class Task05Main {
     }
 
     static int getMin(int a, int b, int c) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        //Если лень или параметров слишком много IntStream.of(a, b, c).min().getAsInt();
+        return a >= b ?
+                b < c ?
+                        b : c :
+                a < c ?
+                        a : c;
     }
 }
