0c6f72e9ce83f08606d878febc253c6e68ee0044
wisedog706 <psnetwork1138@gmail.com>
2019-02-23 10:22:02 +0500

    Task08

--- task08/src/com.example.task08/Task08Main.java
+++ task08/src/com.example.task08/Task08Main.java
@@ -2,14 +2,7 @@ package com.example.task08;
 
 public class Task08Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        int[] arr = new int[2];
-        arr[0] = 2;
-        arr[1] = 3;
-        System.out.println(mult(arr));
-         */
+
     }
 
     static long mult(int[] arr) {
@@ -21,5 +14,4 @@ public class Task08Main {
 
         return comp;
     }
-
 }
