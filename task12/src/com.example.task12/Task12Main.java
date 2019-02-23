8a7d4e0823af870d57d634449bebf8bed9d13a9b
wisedog706 <psnetwork1138@gmail.com>
2019-02-23 11:03:43 +0500

    Task12

--- task12/src/com.example.task12/Task12Main.java
+++ task12/src/com.example.task12/Task12Main.java
@@ -2,17 +2,30 @@ package com.example.task12;
 
 public class Task12Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        int[] arr = {9, 11, 7, 8};
-        selectionSort(arr);
-        System.out.println(java.util.Arrays.toString(arr));
-         */
+
     }
 
     static void selectionSort(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-    }
+        if(arr == null || arr.length == 0) return;
 
+        int tmpmin = arr[0];
+        int indx = 0;
+
+        for(int i = 0; i < arr.length - 1; i++){  //qsort the best
+            for(int j = i; j < arr.length; j++){
+                if(arr[j] < tmpmin) {
+                    tmpmin = arr[j];
+                    indx = j;
+                }
+
+                if(arr[j] == tmpmin)
+                    indx = j;
+            }
+
+            arr[indx] = arr[i];
+            arr[i] = tmpmin;
+            tmpmin = arr[i + 1];
+            indx = 1;
+        }
+    }
 }
