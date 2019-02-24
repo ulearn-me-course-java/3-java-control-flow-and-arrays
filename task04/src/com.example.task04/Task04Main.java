4347e08f8de858280c97f368c29dc493193f7bbe
Platonov2 <jek1999060@mail.ru>
2019-02-24 16:59:51 +0500

    Платонов Евгений. Задание 04

--- task04/src/com.example.task04/Task04Main.java
+++ task04/src/com.example.task04/Task04Main.java
@@ -1,5 +1,7 @@
 package com.example.task04;
 
+import java.io.Console;
+
 public class Task04Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
@@ -10,7 +12,9 @@ public class Task04Main {
     }
 
     static void from0to10000() {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        for (int i = 0; i < 10000; i++) {
+            System.out.println(i);
+        }
     }
 
 }
