5f59e819b11d11cf74f87147c792d7957adae713
Henkka <henkkawar@gmail.com>
2019-02-24 22:34:05 +0500

    task 12

--- task12/src/com.example.task12/Task12Main.java
+++ task12/src/com.example.task12/Task12Main.java
@@ -16,7 +16,7 @@ public class Task12Main {
     }
 
     private static void sortBySelection(int[] arr){
-        for (int i = 0; i < arr.length - 2; i++){
+        for (int i = 0; i < arr.length - 1; i++){
             int min = i;
             for (int j = i + 1; j < arr.length; j++)
                 if(arr[j] < arr[min]) min = j;
