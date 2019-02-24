d13d878e5ffe96b1248ee3b11d45276deb85a2ba
Henkka <henkkawar@gmail.com>
2019-02-24 22:07:32 +0500

    task 11

--- task11/src/com.example.task11/Task11Main.java
+++ task11/src/com.example.task11/Task11Main.java
@@ -12,7 +12,21 @@ public class Task11Main {
     }
 
     static void swap(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (arr != null && arr.length != 0)
+            swapWithZeroIndex(numMin(arr), arr);
+    }
+
+    static void swapWithZeroIndex(int minIndex, int[] arr) {
+        int temp = arr[minIndex];
+        arr[minIndex] = arr[0];
+        arr[0] = temp;
+    }
+
+    static int numMin(int[] arr) {
+        int result = 0;
+        for (int i = 1; i < arr.length; i++)
+            if (arr[i] <= arr[result]) result = i;
+        return result;
     }
 
 }
