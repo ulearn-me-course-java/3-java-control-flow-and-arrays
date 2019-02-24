48607ef8a19b24a5b46cdb20c0c0e324fcba2794
Platonov2 <jek1999060@mail.ru>
2019-02-24 17:38:39 +0500

    Платонов Евгений. Задание 11

--- task11/src/com.example.task11/Task11Main.java
+++ task11/src/com.example.task11/Task11Main.java
@@ -1,5 +1,8 @@
 package com.example.task11;
 
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader;
+import sun.plugin.javascript.navig.Array;
+
 public class Task11Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
@@ -12,7 +15,21 @@ public class Task11Main {
     }
 
     static void swap(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (arr == null || arr.length == 0) return;
+
+        int minIndex = 0;
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] <= min){
+                min = arr[i];
+                minIndex = i;
+            }
+        }
+
+        arr[minIndex] = arr[0];
+        arr[0] = min;
+
     }
 
 }
