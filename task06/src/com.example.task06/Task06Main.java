4b6d0ef901ab99d4baa3c0cf7d8bed9c8c8cfb2d
uytera <uytera12@yandex.ru>
2019-02-26 17:35:37 +0500

    Даниил Бакулин. Задание 06

--- task06/src/com.example.task06/Task06Main.java
+++ task06/src/com.example.task06/Task06Main.java
@@ -10,8 +10,12 @@ public class Task06Main {
     }
 
     static int getMax(int a, int b, int c, int d) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+            int[] temp = {a, b, c, d};
+            int max = Integer.MIN_VALUE;
+            for (int f : temp) {
+                if(max < f)
+                    max = f;
+            }
+            return max;
+        }
     }
-
-}
