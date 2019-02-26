b5db2d0da1e051e149ed32112c9a1ca0d0191ee0
uytera <uytera12@yandex.ru>
2019-02-26 18:01:00 +0500

    Даниил Бакулин. Задание 10

--- task10/src/com.example.task10/Task10Main.java
+++ task10/src/com.example.task10/Task10Main.java
@@ -11,8 +11,16 @@ public class Task10Main {
     }
 
     static int numMin(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        int max = Integer.MAX_VALUE;
+        int counter = 0;
+        for (int i = 0; i < arr.length; i++)
+        {
+            if(max >= arr[i]) {
+                max = arr[i];
+                counter = i;
+            }
+        }
+        return counter;
     }
 
 }
