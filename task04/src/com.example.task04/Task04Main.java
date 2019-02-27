af94646cc938a4449351cfdd8c83bdc4a35f697a
Max <uue33@mail.ru>
2019-02-27 16:49:47 +0500

    Иконников Анастасия. Задание 04

--- task04/src/com.example.task04/Task04Main.java
+++ task04/src/com.example.task04/Task04Main.java
@@ -1,16 +1,20 @@
 package com.example.task04;
+import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class Task04Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         from0to10000();
-         */
+
     }
 
     static void from0to10000() {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        for (int i: IntStream.range(0,10000).toArray()){
+            System.out.println(i);
+        }
     }
 
 }
