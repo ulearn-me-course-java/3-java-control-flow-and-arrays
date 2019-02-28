57143bcd55096500a3a4279f84d3d59a796ea649
Pavel <pavel.zaborin@inbox.ru>
2019-02-28 22:33:28 +0500

    Заборин Павел. Задание 07

--- task07/src/com.example.task07/Task07Main.java
+++ task07/src/com.example.task07/Task07Main.java
@@ -15,8 +15,11 @@ public class Task07Main {
     }
 
     static int sum(int[] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++)
+            sum += arr[i];
         //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        return sum;
     }
 
 }
