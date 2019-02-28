3c2b374a666b81ba6614e579daec7c53770996be
shee4 <lilite71@gmail.com>
2019-03-01 02:41:04 +0500

    task10

--- task10/src/com.example.task10/Task10Main.java
+++ task10/src/com.example.task10/Task10Main.java
@@ -1,5 +1,8 @@
 package com.example.task10;
 
+import java.sql.Array;
+import java.util.Arrays;
+
 public class Task10Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
@@ -11,8 +14,15 @@ public class Task10Main {
     }
 
     static int numMin(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        int res = 0;
+        int min = arr[0];
+        for (int i = 0; i < arr.length; i++){
+            if(arr[i] <= min) {
+                res = i;
+                min = arr[i];
+            }
+        }
+        return res;
     }
 
 }
