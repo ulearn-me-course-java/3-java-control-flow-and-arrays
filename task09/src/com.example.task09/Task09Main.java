810e89d923d42dd46edf39634385e96b23446a74
Pavel <pavel.zaborin@inbox.ru>
2019-02-28 22:46:23 +0500

    Заборин Павел. Задание 09

--- task09/src/com.example.task09/Task09Main.java
+++ task09/src/com.example.task09/Task09Main.java
@@ -14,8 +14,11 @@ public class Task09Main {
     }
 
     static int min(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        int min = arr[0];
+        for (int i = 1; i < arr.length; i++)
+            if (arr[i] < min)
+                min = arr[i];
+        return min;
     }
 
 }
