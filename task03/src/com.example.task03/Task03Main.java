2a93e731fdadc2200a9f302b46ca6de1bb126308
wisedog706 <psnetwork1138@gmail.com>
2019-03-01 09:26:27 +0500

    Task03

--- task03/src/com.example.task03/Task03Main.java
+++ task03/src/com.example.task03/Task03Main.java
@@ -2,31 +2,13 @@ package com.example.task03;
 
 public class Task03Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        System.out.println(getNameOfWeekDays(7));
-         */
+
     }
 
     static String getNameOfWeekDays(int weekDaysNumber) {
-        switch (weekDaysNumber) {
-            case 1 :
-                return "понедельник";
-            case 2:
-                return "вторник";
-            case 3:
-                return "среда";
-            case 4:
-                return "четверг";
-            case 5:
-                return"пятница";
-            case 6:
-                return "суббота";
-            case 7:
-                return "воскресенье";
-            default:
-                return "такого дня недели не существует";
-        }
+        String days[] = {"понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"};
+
+        try { return days[weekDaysNumber - 1]; }
+        catch (Exception ex){ return "incorrect day";}
     }
 }
