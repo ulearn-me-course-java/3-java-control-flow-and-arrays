bd012f6227125988fc4d2d851e376f1112de8bd4
Саша <gorgonova@bk.ru>
2019-03-12 00:18:01 +0500

    Musina Alexandra task07

--- task07/src/com.example.task07/Task07Main.java
+++ task07/src/com.example.task07/Task07Main.java
@@ -15,8 +15,12 @@ public class Task07Main {
     }
 
     static int sum(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        int size = arr.length;
+        int sum = 0;
+        for (int i = 0; i < size; i++) {
+             sum += arr[i];
+        }
+        return sum;
     }
 
 }
