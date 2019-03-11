7e0e3b7b42094206b428a6925f3e183c30edeb4b
Саша <gorgonova@bk.ru>
2019-03-12 00:52:53 +0500

    Musina Alexandra task11

--- task11/src/com.example.task11/Task11Main.java
+++ task11/src/com.example.task11/Task11Main.java
@@ -1,5 +1,7 @@
 package com.example.task11;
 
+import java.lang.reflect.Array;
+
 public class Task11Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
@@ -12,7 +14,21 @@ public class Task11Main {
     }
 
     static void swap(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (arr != null) {
+            int size = arr.length;
+            if (size != 0) {
+                int min = arr[0];
+                int count = 0;
+                for (int i = 1; i < size; i++) {
+                    if (arr[i] <= min) {
+                        min = arr[i];
+                        count = i;
+                    }
+                }
+                arr[count] = arr[0];
+                arr[0] = min;
+            }
+        }
     }
 
 }
