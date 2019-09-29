d86951978a7f2c751695d6cc9a2cc45cba41af7f
6yryndy4una <semeninoff@gmail.com>
2019-09-30 01:25:02 +0500

    [Семенинов] Задание11

--- task11/src/com.example.task11/Task11Main.java
+++ task11/src/com.example.task11/Task11Main.java
@@ -12,7 +12,16 @@ public class Task11Main {
     }
 
     static void swap(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (arr == null || arr.length == 0) return;
+        else {
+            int res = 0;
+            for (int i = 0; i < arr.length; i++)
+                if (arr[res] >= arr[i])
+                    res = i;
+            int tmp = arr[0];
+            arr[0] = arr[res];
+            arr[res] = tmp;
+        }
     }
 
 }
