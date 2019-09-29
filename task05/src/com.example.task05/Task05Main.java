c0c0a8d812e12e87a7ff9cb0ab808b6d38f907cc
6yryndy4una <semeninoff@gmail.com>
2019-09-30 00:04:18 +0500

    [Семенинов] Задание05

--- task05/src/com.example.task05/Task05Main.java
+++ task05/src/com.example.task05/Task05Main.java
@@ -10,7 +10,8 @@ public class Task05Main {
     }
 
     static int getMin(int a, int b, int c) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        int tmp = Math.min(a,b);
+        int res = Math.min(tmp,c);
+        return res;
     }
 }
