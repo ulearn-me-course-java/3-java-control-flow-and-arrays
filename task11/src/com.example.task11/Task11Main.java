b690f801140138f67ff1a33f22200eef9d289f2c
VeronikaMatveeva <VeronicaSomniator@gmail.com>
2019-10-01 13:13:36 +0500

    [Матвеева] Решение задачи 3.11

--- task11/src/com.example.task11/Task11Main.java
+++ task11/src/com.example.task11/Task11Main.java
@@ -1,18 +1,33 @@
 package com.example.task11;
 
+import java.util.Arrays;
+
 public class Task11Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
         int[] arr = {7, 5, 9};
         swap(arr);
         System.out.println(java.util.Arrays.toString(arr));
-         */
     }
 
     static void swap(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (arr == null)
+            System.out.println("если пришедший массив null, то ничего делать не нужно");
+        else if (arr.length == 0)
+            System.out.println("если пришедший массив пуст, то ничего делать не нужно");
+        else {
+            int i = numMin(arr), tmp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = tmp;
+        }
     }
 
+    static int numMin(int[] arr) {
+        int result = 0;
+        for (int i = 0; i < arr.length; i++)
+            if (arr[result] >= arr[i])
+                result = i;
+        return result;
+    }
 }
