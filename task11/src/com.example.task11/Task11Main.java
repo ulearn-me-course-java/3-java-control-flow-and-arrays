8d865445980d270d6f44358fd27b69861177c481
V Kalschikov <vkalschikov@gmail.com>
2019-10-01 11:20:51 +0500

    pass task 11

--- task11/src/com.example.task11/Task11Main.java
+++ task11/src/com.example.task11/Task11Main.java
@@ -12,7 +12,15 @@ public class Task11Main {
     }
 
     static void swap(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (arr==null || arr.length == 0)
+            return;
+        int min = 0;
+        for (int i =0; i<arr.length;i++){
+            min = (arr[min]>=arr[i])?i:min;
+        }
+        arr[0]+=arr[min];
+        arr[min] = arr[0]-arr[min];
+        arr[0]-=arr[min];
     }
 
 }
