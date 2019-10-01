3a8ea0bbbb5215b01cded6417a01fc5e5c6451b7
VeronikaMatveeva <VeronicaSomniator@gmail.com>
2019-10-01 11:19:03 +0500

    [Матвеева] Решение задачи 3.2

--- task02/src/com.example.task02/Task02Main.java
+++ task02/src/com.example.task02/Task02Main.java
@@ -4,12 +4,21 @@ public class Task02Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
         System.out.println(getSeason(12));
-         */
     }
 
     static String getSeason(int monthNumber) {
-        return "";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        switch ((monthNumber % 12) / 3) {
+            case 0:
+                return "зима";
+            case 1:
+                return "весна";
+            case 2:
+                return "лето";
+            case 3:
+                return "осень";
+            default:
+                return null;
+        }
     }
 }
