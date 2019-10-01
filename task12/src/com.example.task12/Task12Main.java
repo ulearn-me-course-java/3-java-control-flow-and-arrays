6aa7e15ee78bf20c480abec48372ec91c0a2d856
V Kalschikov <vkalschikov@gmail.com>
2019-10-01 16:02:58 +0500

    pass task 12

--- task12/src/com.example.task12/Task12Main.java
+++ task12/src/com.example.task12/Task12Main.java
@@ -1,18 +1,33 @@
 package com.example.task12;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Task12Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        int[] arr = {9, 11, 7, 8};
+        int[] arr = {11,10,9,8,7};
         selectionSort(arr);
         System.out.println(java.util.Arrays.toString(arr));
-         */
     }
 
     static void selectionSort(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (arr == null)
+            return;
+        int  index = 0;
+        int[] outArr=new int[arr.length];
+        ArrayList<Integer> list = Arrays.stream(arr).boxed().collect(Collectors.toCollection(ArrayList::new));
+        while(index<arr.length){
+            int min = Collections.min(list);
+            arr[index] = min;
+            index++; list.remove(list.indexOf(min));
+
+        }
     }
 
 }
