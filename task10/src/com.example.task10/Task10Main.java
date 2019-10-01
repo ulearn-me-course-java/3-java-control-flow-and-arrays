9265fabb114546de455428b10dee46f8df976397
V Kalschikov <vkalschikov@gmail.com>
2019-10-01 11:14:53 +0500

    pass task 9

--- task10/src/com.example.task10/Task10Main.java
+++ task10/src/com.example.task10/Task10Main.java
@@ -8,11 +8,17 @@ public class Task10Main {
         int[] arr = {7, 5, 9};
         System.out.println(numMin(arr));
          */
+        int[] arr = {0,0,0,0};
+        System.out.println(numMin(arr));
     }
 
     static int numMin(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        int min = 0;
+        for(int i =0; i<arr.length;i++){
+            if (arr[min] >= arr[i])
+                min = i;
+        }
+        return min;
     }
 
 }
