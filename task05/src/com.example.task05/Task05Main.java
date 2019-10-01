98dafa2089886d29905254d82656af45c1c26da4
Валерия Самсонова <samsonova147@hotmail.com>
2019-10-01 15:08:58 +0500

    [Самсонова] Решение задачи 3.5

--- task05/src/com.example.task05/Task05Main.java
+++ task05/src/com.example.task05/Task05Main.java
@@ -4,13 +4,17 @@ public class Task05Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         System.out.println(getMin(5,4,3));
-         */
+
     }
 
     static int getMin(int a, int b, int c) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        int[] arr = {a, b, c};
+        int min = arr[0];
+        for (int i = 0; i != arr.length; i++)
+            if (arr[i] < min)
+                min = arr[i];
+        return min;
     }
 }
