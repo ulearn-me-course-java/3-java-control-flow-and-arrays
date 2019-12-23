059e8ec70f79486f72154c479e426747189eb6a6
rokoman13 <rokoman13@users.noreply.github.com>
2019-12-23 13:39:16 +0500

    Popov Evgeny task02

--- task02/src/com.example.task02/Task02Main.java
+++ task02/src/com.example.task02/Task02Main.java
@@ -10,6 +10,18 @@ public class Task02Main {
     }
 
     static String getSeason(int monthNumber) {
+        if ((monthNumber >= 1 && monthNumber <= 2) || monthNumber == 12) {
+            return "зима";
+        }
+        else if (monthNumber >= 3 && monthNumber <= 5){
+            return "весна";
+        }
+        else if (monthNumber >= 6 && monthNumber <= 8){
+            return "лето";
+        }
+        else if (monthNumber >= 9 && monthNumber <= 11){
+            return "осень";
+        }
         return "";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
     }
 }
