3f3148cc218efb4095857db9fcd575070b324c13
rokoman13 <rokoman13@users.noreply.github.com>
2019-12-23 13:41:09 +0500

    Popov Evgeny task05

--- task05/src/com.example.task05/Task05Main.java
+++ task05/src/com.example.task05/Task05Main.java
@@ -10,7 +10,10 @@ public class Task05Main {
     }
 
     static int getMin(int a, int b, int c) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        int min = Integer.MAX_VALUE;
+        min = Integer.min(a, min);
+        min = Integer.min(b, min);
+        min = Integer.min(c, min);
+        return min;
     }
 }
