1753d0f4d0dea3a3c1e36c46ee1263d0241d88b0
yeroxo <yeroxo@gmail.com>
2020-02-28 10:55:40 +0500

    Колыванов Артем ПрИ 201 Задание 3.10

--- task10/src/com.example.task10/Task10Main.java
+++ task10/src/com.example.task10/Task10Main.java
@@ -1,5 +1,7 @@
 package com.example.task10;
 
+import java.util.Arrays;
+
 public class Task10Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
@@ -11,8 +13,14 @@ public class Task10Main {
     }
 
     static int numMin(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        int index = 0;
+        int[] copy = Arrays.copyOf(arr,arr.length);
+        Arrays.sort(copy);
+        for (int i = 0; i < arr.length ; i++) {
+            if (arr[i] == copy[0]) {
+                index = i;
+            }
+        }
+        return index;
     }
-
 }
