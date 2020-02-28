aed071760b10874b1931ca9581e3d0deaafe1b5e
yeroxo <yeroxo@gmail.com>
2020-02-28 10:57:25 +0500

    Колыванов Артем ПрИ 201 Задание 3.11

--- task11/src/com.example.task11/Task11Main.java
+++ task11/src/com.example.task11/Task11Main.java
@@ -1,18 +1,26 @@
 package com.example.task11;
 
+import java.util.Arrays;
+
 public class Task11Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        int[] arr = {7, 5, 9};
-        swap(arr);
-        System.out.println(java.util.Arrays.toString(arr));
-         */
+
     }
 
-    static void swap(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+    static void swap( int[] arr) {
+        int index = 0;
+        if ((arr != null) && (arr.length != 0)) {
+            int[] copy = Arrays.copyOf(arr, arr.length);
+            Arrays.sort(copy);
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] == copy[0]) {
+                    index = i;
+                }
+            }
+            int lowest = copy[0];
+            int first = arr[0];
+            arr[index] = first;
+            arr[0] = lowest;
+        }
     }
-
 }
