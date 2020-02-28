cf81b5f49d7392c51c34fd20430db45a1300273a
yeroxo <yeroxo@gmail.com>
2020-02-28 10:59:11 +0500

    Колыванов Артем ПрИ 201 Задание 3.12

--- task12/src/com.example.task12/Task12Main.java
+++ task12/src/com.example.task12/Task12Main.java
@@ -2,17 +2,25 @@ package com.example.task12;
 
 public class Task12Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        int[] arr = {9, 11, 7, 8};
-        selectionSort(arr);
-        System.out.println(java.util.Arrays.toString(arr));
-         */
+
     }
 
     static void selectionSort(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if ((arr != null) && (arr.length != 0)) {
+            for (int k = 0; k < arr.length; k++) {
+                int index = 0;
+                int min = Integer.MAX_VALUE;
+                for (int i = k; i < arr.length; i++) {
+                    if (arr[i] <= min) {
+                        min = arr[i];
+                        index = i;
+                    }
+                }
+                int first = arr[k];
+                arr[index] = first;
+                arr[k] = min;
+            }
+        }
     }
 
 }
