1561dde281c7129eae2769f76118b45c6a1f7cf4
DarenOwl <Charly.Fox@mail.ru>
2020-02-29 20:53:13 +0500

    Андреева Юлия. Задание12

--- task13/src/com.example.task13/Task13Main.java
+++ task13/src/com.example.task13/Task13Main.java
@@ -1,5 +1,7 @@
 package com.example.task13;
 
+import java.util.ArrayList;
+
 public class Task13Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
@@ -12,8 +14,39 @@ public class Task13Main {
     }
 
     static int[] removeMoreThen1000(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return  null;
+        if (arr == null)
+            return null;
+
+        ArrayList<Integer> filteredArr = new ArrayList<>();
+        for (int x : arr) {
+            if (x < 1000)
+                filteredArr.add(x);
+        }
+
+        int[] result = new int[filteredArr.size()];
+        int i = 0;
+        for (int x : filteredArr) {
+            result[i++] = x;
+        }
+
+        return result;
+    }
+
+    static int[] removeMoreThen1000variant2(int[] arr) {
+        if (arr == null)
+            return null;
+        int count1000 = 0;
+        for (int x : arr){
+            if (x >= 1000)
+                count1000++;
+        }
+        int[] result = new int[arr.length - count1000];
+        int i = 0;
+        for (int x : arr){
+            if (x < 1000)
+                result[i++] = x;
+        }
+        return  result;
     }
 
 }
