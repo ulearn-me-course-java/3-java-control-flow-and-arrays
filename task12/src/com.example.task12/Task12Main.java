161219b98838a2263f0fea26b4684c2b48b1250d
Dauren <baidau.rk.a@gmail.com>
2020-03-01 02:57:05 +0500

    Байманов Даурен.Задание 12

--- task12/src/com.example.task12/Task12Main.java
+++ task12/src/com.example.task12/Task12Main.java
@@ -11,8 +11,22 @@ public class Task12Main {
          */
     }
 
+    static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
     static void selectionSort(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if(arr==null) return;
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < n; j++)
+                if (arr[j] < arr[min])
+                    min = j;
+            swap(arr, min, i);
+        }
     }
 
 }
