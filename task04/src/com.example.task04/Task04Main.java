397f583a5757bf3adec5567a9ab48276c863cd34
Dauren <baidau.rk.a@gmail.com>
2020-03-01 18:32:07 +0500

    Байманов Даурен.Задание 04

--- task04/src/com.example.task04/Task04Main.java
+++ task04/src/com.example.task04/Task04Main.java
@@ -1,18 +1,17 @@
 package com.example.task04;
 
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class Task04Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+
         from0to10000();
-         */
+
     }
 
     static void from0to10000() {
-        for (int i = 0; i < 10000; i++) {
-            System.out.println(i);
-        }
+        IntStream.range(0, 10000).peek(o -> System.out.println(o)).toArray();
     }
 
 }
