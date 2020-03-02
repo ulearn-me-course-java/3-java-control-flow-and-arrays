6b8e0f72d25dfcfd635c3e27984b904b78aab0de
Grin <grinlikegreenbutgrin@gmail.com>
2020-03-02 10:40:13 +0500

    task #12

--- task12/src/com.example.task12/Task12Main.java
+++ task12/src/com.example.task12/Task12Main.java
@@ -2,17 +2,34 @@ package com.example.task12;
 
 public class Task12Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         int[] arr = {9, 11, 7, 8};
         selectionSort(arr);
         System.out.println(java.util.Arrays.toString(arr));
-         */
     }
 
     static void selectionSort(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (arr == null || arr.length == 0) return;
+        for (int i = 0; i < arr.length; i++){
+            swap(arr, i);
+        }
+    }
+
+    static void swap(int[] arr, int startInd) {
+        int i = numMin(arr, startInd);
+        int buf = arr[i];
+        arr[i] = arr[startInd];
+        arr[startInd] = buf;
+    }
+
+    static int numMin(int[] arr, int startInd) {
+        int minInd = 0;
+        int minVal = Integer.MAX_VALUE;
+        for (int i = startInd; i < arr.length; i++)
+            if (arr[i] <= minVal) {
+                minInd = i;
+                minVal = arr[i];
+            }
+        return minInd;
     }
 
 }
