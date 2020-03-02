e52707bf6c75184bf401e9ba9905ac9dba500290
Unknown <unknown@example.com>
2020-03-03 01:46:38 +0500

    task12

--- task12/src/com.example.task12/Task12Main.java
+++ task12/src/com.example.task12/Task12Main.java
@@ -2,17 +2,29 @@ package com.example.task12;
 
 public class Task12Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+
         int[] arr = {9, 11, 7, 8};
         selectionSort(arr);
         System.out.println(java.util.Arrays.toString(arr));
-         */
+
     }
 
     static void selectionSort(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        int temp;
+        if(arr != null)
+        for (int i = 0; i < arr.length - 1; i++)
+        {
+            for (int j = 0; j < arr.length - 1; j++)
+            {
+                if (arr[j + 1] < arr[j])
+                {
+                    temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
     }
+
 
 }
