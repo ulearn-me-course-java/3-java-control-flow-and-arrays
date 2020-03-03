1d65fcb0812befec319514a9e2d343d15a1a95ae
Elizaveta Trushnikova <etsatsko@mail.ru>
2020-03-03 13:51:33 +0500

    Трушникова Елизавета. Задание 09

---
+++ task09/src/com.example.task09/com/example/task09/Task09Main.java
@@ -0,0 +1,20 @@
+package com.example.task09;
+
+public class Task09Main {
+    public static void main(String[] args) {
+        int[] arr = new int[3];
+        arr[0] = 2;
+        arr[1] = -1;
+        arr[2] = 3;
+        System.out.println(min(arr));
+    }
+
+    static int min(int[] arr) {
+        int min = Integer.MAX_VALUE;
+        for (int e: arr) {
+            if (e < min)
+                min = e;
+        }
+        return min;
+    }
+}
