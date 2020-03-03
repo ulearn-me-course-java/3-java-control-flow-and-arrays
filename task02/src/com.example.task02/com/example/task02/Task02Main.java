23a582e489f3bc2409d467973f5c7359ad03b362
Elizaveta Trushnikova <etsatsko@mail.ru>
2020-03-03 11:45:18 +0500

    Трушникова Елизавета. Задание 02

---
+++ task02/src/com.example.task02/com/example/task02/Task02Main.java
@@ -0,0 +1,20 @@
+package com.example.task02;
+
+public class Task02Main {
+    public static void main(String[] args) {
+        System.out.println(getSeason(12));
+    }
+
+    static String getSeason(int monthNumber) {
+        if (monthNumber < 1 || monthNumber > 12)
+            return "не корректно введен номер месяца";
+        else if (monthNumber >=3 && monthNumber <= 5)
+            return "весна";
+        else if  (monthNumber >=6 && monthNumber <= 8)
+            return "лето";
+        else if  (monthNumber >=9 && monthNumber <= 11)
+            return "осень";
+        else
+            return "зима";
+    }
+}
