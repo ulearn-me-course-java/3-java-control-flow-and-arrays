65fc05a76b835556e3650b0a8ee0d7654797ee5e
Elizaveta Trushnikova <etsatsko@mail.ru>
2020-03-03 14:02:49 +0500

    Трушникова Елизавета. Задание 10

---
+++ task10/src/com.example.task10/com/example/task10/Task10Main.java
@@ -0,0 +1,20 @@
+package com.example.task10;
+
+public class Task10Main {
+    public static void main(String[] args) {
+        int[] arr = {7, 5, 9};
+        System.out.println(numMin(arr));
+    }
+
+    static int numMin(int[] arr) {
+        int min = Integer.MAX_VALUE;
+        int k = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] <= min ) {
+                min = arr[i];
+                k = i;
+            }
+        }
+        return k;
+    }
+}
