808aed4f89e26debd1b435c2ecede263216dcab3
Elizaveta Trushnikova <etsatsko@mail.ru>
2020-03-03 13:19:15 +0500

    Трушникова Елизавета. Задание 06

---
+++ task06/src/com.example.task06/com/example/task06/Task06Main.java
@@ -0,0 +1,11 @@
+package com.example.task06;
+
+public class Task06Main {
+    public static void main(String[] args) {
+        System.out.println(getMax(1, 2, 3, 4));
+    }
+
+    static int getMax(int a, int b, int c, int d) {
+        return Math.max(Math.max(a,b),Math.max(c,d));
+    }
+}
