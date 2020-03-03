175d6d9cf99179ad1700db392b789fff70f15aaa
Elizaveta Trushnikova <etsatsko@mail.ru>
2020-03-03 13:27:19 +0500

    Трушникова Елизавета. Задание 07

---
+++ task07/src/com.example.task07/com/example/task07/Task07Main.java
@@ -0,0 +1,21 @@
+package com.example.task07;
+
+public class Task07Main {
+    public static void main(String[] args) {
+        int[] myArray = new int[4];
+        myArray[0] = 2;
+        myArray[1] = 10;
+        myArray[2] = 10;
+        myArray[3] = 0;
+        System.out.println(sum(myArray));
+    }
+
+    static int sum(int[] arr) {
+        int sum = 0;
+        for(int e : arr) {
+            sum += e;
+        }
+        return sum;
+    }
+
+}
