99cb3096aee0f47b5948fe68f6baa924ad306ae3
Elizaveta Trushnikova <etsatsko@mail.ru>
2020-03-03 13:04:19 +0500

    Трушникова Елизавета. Задание 04

---
+++ task04/src/com.example.task04/com/example/task04/Task04Main.java
@@ -0,0 +1,13 @@
+package com.example.task04;
+
+public class Task04Main {
+    public static void main(String[] args) {
+        from0to10000();
+    }
+
+    static void from0to10000() {
+        for (int i = 0; i < 10000; i++) {
+            System.out.println(i);
+        }
+    }
+}
