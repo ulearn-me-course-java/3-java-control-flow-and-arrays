485108cf7424a617c355545a2fcd252e6a1df664
Elizaveta Trushnikova <etsatsko@mail.ru>
2020-03-03 13:41:58 +0500

    Трушникова Елизавета. Задание 08

---
+++ task08/src/com.example.task08/com/example/task08/Task08Main.java
@@ -0,0 +1,21 @@
+package com.example.task08;
+
+public class Task08Main {
+    public static void main(String[] args) {
+        int[] arr = new int[2];
+        arr[0] = 2;
+        arr[1] = 3;
+        System.out.println(mult(arr));
+    }
+
+    static long mult(int[] arr) {
+        if (arr.length == 0)
+            return 0;
+
+        long mult = 1;
+        for (int e: arr){
+            mult *= e;
+        }
+        return mult;
+    }
+}
