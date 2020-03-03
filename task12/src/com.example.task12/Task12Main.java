5d99a14e08ba5b0dd1374e5a9623e852b8189b7a
Ekaterina <malyavkina-00@mail.ru>
2020-03-03 22:24:00 +0500

    Малявкина Екатерина Задание №12

--- task12/src/com.example.task12/Task12Main.java
+++ task12/src/com.example.task12/Task12Main.java
@@ -12,7 +12,23 @@ public class Task12Main {
     }
 
     static void selectionSort(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (arr == null)
+            return;
+
+        for (int i = 0; i < arr.length; i++) {
+            int index = i;
+            int min = Integer.MAX_VALUE;
+            //поиск минимального элемента справа от текущего
+            for (int j = i; j < arr.length; j++) {
+                if (arr[j] < min) {
+                    min = arr[j];
+                    index = j;
+                }
+            }
+            //минимальный и текущий меняем местами
+            arr[index] = arr[i];
+            arr[i] = min;
+        }
     }
 
 }
