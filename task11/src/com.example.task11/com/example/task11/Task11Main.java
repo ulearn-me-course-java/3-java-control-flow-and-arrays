079bb6a0321574573e25c3e5231fd2bdf09a2c5c
Elizaveta Trushnikova <etsatsko@mail.ru>
2020-03-04 20:09:24 +0500

    Трушникова Елизавета. Задание 11

---
+++ task11/src/com.example.task11/com/example/task11/Task11Main.java
@@ -0,0 +1,25 @@
+package com.example.task11;
+
+public class Task11Main {
+    public static void main(String[] args) {
+        int[] arr = {7, 5, 9};
+        swap(arr);
+        System.out.println(java.util.Arrays.toString(arr));
+    }
+
+    static void swap(int[] arr) {
+        if (arr == null || arr.length == 0)
+            return;
+        int min = Integer.MAX_VALUE;
+        int index = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+                index = i;
+            }
+        }
+        arr[index] = arr[0];
+        arr[0] = min;
+    }
+
+}
