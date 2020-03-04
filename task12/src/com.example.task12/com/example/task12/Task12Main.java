5ba5757a3dda646ebea37414f8b8c68ea580858a
Elizaveta Trushnikova <etsatsko@mail.ru>
2020-03-04 20:17:28 +0500

    Трушникова Елизавета. Задание 12

---
+++ task12/src/com.example.task12/com/example/task12/Task12Main.java
@@ -0,0 +1,29 @@
+package com.example.task12;
+
+public class Task12Main {
+    public static void main(String[] args) {
+        int[] arr = {9, 11, 7, 8};
+        selectionSort(arr);
+        System.out.println(java.util.Arrays.toString(arr));
+    }
+
+    static void selectionSort(int[] arr) {
+        if (arr == null || arr.length == 0)
+            return;
+
+        for (int i = 0; i < arr.length; i++){
+            int min = Integer.MAX_VALUE;
+            int index = 0;
+
+            for (int j = i; j < arr.length; j++) {
+                if (arr[j] < min) {
+                    min = arr[j];
+                    index = j;
+                }
+            }
+
+            arr[index] = arr[i];
+            arr[i] = min;
+        }
+    }
+}
