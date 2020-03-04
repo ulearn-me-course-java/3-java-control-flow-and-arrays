46e8a5763f041c0e01684ba09440a253404a0a64
MatveyTolpeev <61470886+MatveyTolpeev@users.noreply.github.com>
2020-03-04 21:30:22 +0500

    Толпеев 3.12

--- task12/src/com.example.task12/Task12Main.java
+++ task12/src/com.example.task12/Task12Main.java
@@ -13,6 +13,21 @@ public class Task12Main {
 
     static void selectionSort(int[] arr) {
         //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if(arr == null || arr.length == 0) return;
+
+        int minIndex = 0;
+        for(int i = 0;i < arr.length;i++)
+        {
+            for(int j = i;j < arr.length;j++)
+            {
+                if(arr[minIndex] > arr[j])
+                    minIndex = j;
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+            minIndex = i + 1;
+        }
     }
 
 }
