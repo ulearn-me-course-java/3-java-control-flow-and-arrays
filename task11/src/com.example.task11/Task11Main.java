7b1714d3778dcdabf0586c81348a3f0254263d61
hackerzhenya <zhenya92.ru@gmail.com>
2020-03-06 01:38:58 +0500

    task 11

--- task11/src/com.example.task11/Task11Main.java
+++ task11/src/com.example.task11/Task11Main.java
@@ -1,18 +1,29 @@
 package com.example.task11;
 
+import java.util.stream.IntStream;
+
 public class Task11Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         int[] arr = {7, 5, 9};
         swap(arr);
         System.out.println(java.util.Arrays.toString(arr));
-         */
     }
 
     static void swap(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (arr != null && arr.length > 0)
+            swap(arr, 0, minIndex(arr));
     }
 
+    static void swap(int[] arr, int first_idx, int second_idx) {
+        int temp = arr[first_idx];
+        arr[first_idx] = arr[second_idx];
+        arr[second_idx] = temp;
+    }
+
+    static int minIndex(int[] arr) {
+        return IntStream
+                .range(0, arr.length)
+                .reduce((i, j) -> arr[i] < arr[j] ? i : j)
+                .orElse(0);
+    }
 }
