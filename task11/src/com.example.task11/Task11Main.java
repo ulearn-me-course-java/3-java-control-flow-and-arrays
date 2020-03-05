15c532ca695ba951c44ed1a7d8ba581f88db7478
Наби Гадимов <gadimovnabi@gmail.com>
2020-03-06 00:34:29 +0500

    Гадимов Наби. Задание 3.11

--- task11/src/com.example.task11/Task11Main.java
+++ task11/src/com.example.task11/Task11Main.java
@@ -4,15 +4,32 @@ public class Task11Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         int[] arr = {7, 5, 9};
         swap(arr);
         System.out.println(java.util.Arrays.toString(arr));
-         */
+
     }
 
     static void swap(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (arr == null || arr.length == 0)
+        {
+            return;
+        }
+        int firstNum = arr[0];
+        int min1 = arr[0];
+        int num = 0;
+        for (int i = 0; i < arr.length; i++)
+        {
+            if (min1 > arr[i])
+            {
+                min1 = arr[i];
+                num = i;
+            }
+        }
+        arr[0] = min1;
+        arr[num] = firstNum;
     }
+
 
 }
