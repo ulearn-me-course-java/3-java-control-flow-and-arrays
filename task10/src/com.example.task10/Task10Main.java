c9dc62b39a2ccdde7abad46206226c1a98a20e34
hackerzhenya <zhenya92.ru@gmail.com>
2020-03-06 01:30:20 +0500

    task 10

--- task10/src/com.example.task10/Task10Main.java
+++ task10/src/com.example.task10/Task10Main.java
@@ -1,18 +1,20 @@
 package com.example.task10;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Task10Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         int[] arr = {7, 5, 9};
         System.out.println(numMin(arr));
-         */
     }
 
     static int numMin(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        return Arrays
+                .stream(arr)
+                .boxed()
+                .collect(Collectors.toList())
+                .lastIndexOf(IntStream.of(arr).min().getAsInt());
     }
-
 }
