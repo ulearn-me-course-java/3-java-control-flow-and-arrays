1a90fc4ba6e8b0150e81ca4f71eacd7a5d679df7
Наби Гадимов <gadimovnabi@gmail.com>
2020-03-05 23:00:46 +0500

    Гадимов Наби. Задание 3.5

--- task06/src/com.example.task06/Task06Main.java
+++ task06/src/com.example.task06/Task06Main.java
@@ -4,14 +4,24 @@ public class Task06Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         System.out.println(getMax(1, 2, 3, 4));
-         */
+
     }
 
     static int getMax(int a, int b, int c, int d) {
         //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        return Math.min(a,Math.min(b,c));
     }
 
 }
+/* другой метод:
+        if(a < b && a < c){
+            return a;
+        }
+        else if(b > a && b > c){
+            return b;
+        }
+        return c;
+
+ */
