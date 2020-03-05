79af6bc20855484ed1519c3636b6006df724198b
hackerzhenya <zhenya92.ru@gmail.com>
2020-03-06 01:52:45 +0500

    task 12

--- task12/src/com.example.task12/Task12Main.java
+++ task12/src/com.example.task12/Task12Main.java
@@ -1,18 +1,32 @@
 package com.example.task12;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class Task12Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         int[] arr = {9, 11, 7, 8};
         selectionSort(arr);
-        System.out.println(java.util.Arrays.toString(arr));
-         */
+        System.out.println(Arrays.toString(arr));
     }
 
     static void selectionSort(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (arr != null && arr.length > 0)
+            IntStream
+                    .range(0, arr.length)
+                    .forEach(idx -> swap(arr, idx, indexOfMin(arr, idx)));
     }
 
+    static void swap(int[] array, int first, int second) {
+        int temp = array[first];
+        array[first] = array[second];
+        array[second] = temp;
+    }
+
+    static int indexOfMin(int[] array, int from) {
+        return IntStream
+                .range(from, array.length)
+                .reduce((i, j) -> array[i] < array[j] ? i : j)
+                .getAsInt();
+    }
 }
