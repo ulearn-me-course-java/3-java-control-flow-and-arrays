4ad4aac445755419ea4bcc27948eddeff379d1dc
xhnrdt00n <xoine.un@gmail.com>
2020-03-05 22:20:55 +0500

    Жмакина Алина. Задание 3.11

--- task11/src/com.example.task11/Task11Main.java
+++ task11/src/com.example.task11/Task11Main.java
@@ -12,7 +12,18 @@ public class Task11Main {
     }
 
     static void swap(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (arr == null || arr.length == 0) return;
+        int firstNum = arr[0];
+        int minNum = arr[0];
+        int minIndex = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (minNum > arr[i]) {
+                minNum = arr[i];
+                minIndex = i;
+            }
+        }
+        arr[0] = minNum;
+        arr[minIndex] = firstNum;
     }
 
 }
