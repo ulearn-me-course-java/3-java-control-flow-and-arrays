6d04f6cf8d3cfdd0323fda8e54638322978ada3f
Наби Гадимов <gadimovnabi@gmail.com>
2020-03-06 00:27:07 +0500

    Гадимов Наби. Задание 3.10

--- task10/src/com.example.task10/Task10Main.java
+++ task10/src/com.example.task10/Task10Main.java
@@ -4,22 +4,24 @@ public class Task10Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         int[] arr = {7, 5, 9};
         System.out.println(numMin(arr));
-         */
+
     }
 
     static int numMin(int[] arr) {
+
         int min1 = arr[0];
         int num = 0;
         for (int i = 1; i < arr.length; i++)
         {
-            if (arr[i] <= min1)
+            if (min1 >= arr[i])
+            {
                 min1 = arr[i];
-            num = i;
+                num = i;
+            }
         }
         return num;
     }
-
 }
