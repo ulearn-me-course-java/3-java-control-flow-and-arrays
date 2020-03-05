f0a5fa3cf15fda570d19536a7ae8a89df9696aa4
Наби Гадимов <gadimovnabi@gmail.com>
2020-03-06 00:44:48 +0500

    Гадимов Наби. Задание 3.12

--- task12/src/com.example.task12/Task12Main.java
+++ task12/src/com.example.task12/Task12Main.java
@@ -4,15 +4,34 @@ public class Task12Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         int[] arr = {9, 11, 7, 8};
         selectionSort(arr);
         System.out.println(java.util.Arrays.toString(arr));
-         */
+
     }
 
     static void selectionSort(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (arr == null || arr.length == 0)
+        {
+            return;
+        }
+        int min1;
+        int index;
+        for (int i = 0; i<arr.length; i++)
+        {
+            min = arr[i];
+            index=i;
+            for (int j = i; j < arr.length; j++) {
+                if (arr[j] < min1) {
+                    min1 = arr[j];
+                    index = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = min1;
+            arr[index] = temp;
+        }
     }
 
 }
