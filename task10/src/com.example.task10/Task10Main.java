4a0d1ab86db5bedcc669d8ca30ffb5bea6c9bd6e
xhnrdt00n <xoine.un@gmail.com>
2020-03-05 22:02:17 +0500

    Жмакина Алина. Задание 3.10

--- task10/src/com.example.task10/Task10Main.java
+++ task10/src/com.example.task10/Task10Main.java
@@ -11,8 +11,15 @@ public class Task10Main {
     }
 
     static int numMin(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        int min = arr[0];
+        int minNumber = 0;
+        for (int i = 1; i < arr.length; i++){
+            if (min >= arr[i]){
+                min = arr[i];
+                minNumber = i;
+            }
+        }
+        return minNumber;
     }
 
 }
