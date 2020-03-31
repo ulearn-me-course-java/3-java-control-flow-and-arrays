3087d4c0bc030e21aef7fb77c5d443e58abbeb69
Ekaterina <slalam.ww@gmail.com>
2020-03-31 12:51:53 +0500

    Абрамкова, 3.11

--- task11/src/com.example.task11/Task11Main.java
+++ task11/src/com.example.task11/Task11Main.java
@@ -2,17 +2,24 @@ package com.example.task11;
 
 public class Task11Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         int[] arr = {7, 5, 9};
         swap(arr);
         System.out.println(java.util.Arrays.toString(arr));
-         */
     }
 
     static void swap(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if(arr==null || arr.length==0) return;
+
+        int min = arr[0]; //минимальный элемент
+        int num = 0;
+        for(int i = 0; i<arr.length; i++)
+            if(arr[i] <= min){
+                min = arr[i];
+                num = i;
+            }
+        int qwe = arr[0];
+        arr[0] = min;
+        arr[num] = qwe;
     }
 
 }
