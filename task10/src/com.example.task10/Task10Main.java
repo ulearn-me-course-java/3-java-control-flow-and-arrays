31c69c3e42e0f3dd71f31659aa31dfd8a4693768
Ekaterina <slalam.ww@gmail.com>
2020-03-31 12:12:30 +0500

    Абрамкова, 3.10

--- task10/src/com.example.task10/Task10Main.java
+++ task10/src/com.example.task10/Task10Main.java
@@ -2,17 +2,25 @@ package com.example.task10;
 
 public class Task10Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         int[] arr = {7, 5, 9};
         System.out.println(numMin(arr));
-         */
     }
 
     static int numMin(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        //вернуть номер минимального элемента, если несколько, то последнего
+        int num = 0; //номер сравниваемого с numMin
+        int numMin = 0; //номер минимального элемента
+        int min = arr[0]; //минимальный элемент
+
+        while (num < arr.length){
+            if (arr[num] <= min){
+                min = arr[num]; //новый numMin
+                numMin = num;
+                num++;
+            }
+            else num++;
+        }
+        return numMin;
     }
 
 }
