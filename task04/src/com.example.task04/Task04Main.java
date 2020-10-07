a564b8279e24db3c320fc021f669836c47dac5aa
Евгений <Evgeniy.vm11@gmail.com>
2020-10-07 20:57:55 +0500

    Михайловский Евгений 3.4

--- task04/src/com.example.task04/Task04Main.java
+++ task04/src/com.example.task04/Task04Main.java
@@ -1,16 +1,18 @@
 package com.example.task04;
 
+import java.util.stream.IntStream;
+
 public class Task04Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         from0to10000();
-         */
+
     }
 
     static void from0to10000() {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        IntStream.range(0, 10000)
+                .forEach(System.out::println);
     }
-
 }
