41c5a0e9d4c60ce1f93c96109e1658f319c11ef2
Евгений <Evgeniy.vm11@gmail.com>
2020-10-07 20:55:56 +0500

    Михайловский Евгений 3.3

--- task03/src/com.example.task03/Task03Main.java
+++ task03/src/com.example.task03/Task03Main.java
@@ -4,12 +4,33 @@ public class Task03Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         System.out.println(getNameOfWeekDays(7));
-         */
     }
 
     static String getNameOfWeekDays(int weekDaysNumber) {
-        return "";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        switch (weekDaysNumber){
+            case 1: return "понедельник";
+            case 2: return "вторник";
+            case 3: return "среда";
+            case 4: return "четверг";
+            case 5: return "пятница";
+            case 6: return "суббота";
+            case 7: return "воскресенье";
+            default:return "такого дня недели не существуе";
+        }
+
+        /*
+        if (weekDaysNumber < 1 && weekDaysNumber > 7) return "такого дня недели не существуе";
+        else if (weekDaysNumber == 1) return "понедельник";
+        else if (weekDaysNumber == 2) return "вторник";
+        else if (weekDaysNumber == 3) return "среда";
+        else if (weekDaysNumber == 4) return "четверг";
+        else if (weekDaysNumber == 5) return "пятница";
+        else if (weekDaysNumber == 6) return "суббота";
+        else if (weekDaysNumber == 7) return "воскресенье";
+        return "";
+         */
+
     }
 }
