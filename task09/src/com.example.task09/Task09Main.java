7b3b57cea4df71ad9ac3344b9847cd0c7bde34d8
PavelLediankin <pavel.lediankin@at.urfu.ru>
2020-11-06 16:20:27 +0500

    Ледянкин Павел. Задание 3.09

--- task09/src/com.example.task09/Task09Main.java
+++ task09/src/com.example.task09/Task09Main.java
@@ -1,21 +1,22 @@
 package com.example.task09;
 
-public class Task09Main {
-    public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+public class Task09Main
+{
+    public static void main(String[] args)
+    {
         int[] arr = new int[3];
         arr[0] = 2;
         arr[1] = -1;
         arr[2] = 3;
         System.out.println(min(arr));
-         */
     }
 
-    static int min(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+    static int min(int[] arr)
+    {
+        int min = Integer.MAX_VALUE;
+        for (int v : arr)
+            if (v < min)
+                min = v;
+        return min;
     }
-
 }
