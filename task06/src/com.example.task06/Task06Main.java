50a1f9dbf50a152f22e33218ac85ee7010c8bfb8
PavelLediankin <pavel.lediankin@at.urfu.ru>
2020-11-06 16:06:55 +0500

    Ледянкин Павел. Задание 3.06

--- task06/src/com.example.task06/Task06Main.java
+++ task06/src/com.example.task06/Task06Main.java
@@ -1,17 +1,14 @@
 package com.example.task06;
 
-public class Task06Main {
-    public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+public class Task06Main
+{
+    public static void main(String[] args)
+    {
         System.out.println(getMax(1, 2, 3, 4));
-         */
     }
 
-    static int getMax(int a, int b, int c, int d) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+    static int getMax(int a, int b, int c, int d)
+    {
+        return Math.max(Math.max(a, b), Math.max(c, d));
     }
-
 }
