97726f4154cd4bc8f2d11bc0243135c268390f1a
M <mihail.petuxov@yandex.ru>
2021-02-18 15:45:59 +0500

    Петухов Михаил. Задача 3.10

--- task10/src/com.example.task10/Task10Main.java
+++ task10/src/com.example.task10/Task10Main.java
@@ -1,5 +1,7 @@
 package com.example.task10;
 
+import java.util.Arrays;
+
 public class Task10Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
@@ -11,8 +13,11 @@ public class Task10Main {
     }
 
     static int numMin(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        int minI = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] <= arr[i-1])
+                minI = i;
+        }
+        return minI;
     }
-
 }
