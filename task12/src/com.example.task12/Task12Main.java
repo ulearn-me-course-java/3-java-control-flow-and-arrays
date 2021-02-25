ba5f471911125410ba8bda6d2dc306a301b7b973
Darina <37955869+dagenator@users.noreply.github.com>
2021-02-25 12:35:09 +0500

    task12

--- task12/src/com.example.task12/Task12Main.java
+++ task12/src/com.example.task12/Task12Main.java
@@ -4,15 +4,25 @@ public class Task12Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         int[] arr = {9, 11, 7, 8};
         selectionSort(arr);
         System.out.println(java.util.Arrays.toString(arr));
-         */
+
     }
 
     static void selectionSort(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if(arr == null || arr.length ==0 ) return;
+
+        for( int i =0; i< arr.length; i++ ){
+            int indexMin = arr.length-1;
+            for(int v = i ; v < arr.length; v++ ){
+                if(arr[indexMin] >  arr[v]) indexMin = v;
+            }
+            int min = arr[indexMin];
+            arr[indexMin] = arr[i];
+            arr[i] = min;
+        }
     }
 
 }
