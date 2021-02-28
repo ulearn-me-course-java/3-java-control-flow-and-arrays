828b91f95d716225534351548070b7a04afc8c87
Wither87 <wither_87@mail.ru>
2021-02-28 18:26:23 +0500

    Крутолевич Сергей Задание 3.6

--- task06/src/com.example.task06/Task06Main.java
+++ task06/src/com.example.task06/Task06Main.java
@@ -10,8 +10,11 @@ public class Task06Main {
     }
 
     static int getMax(int a, int b, int c, int d) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        int max = a;
+        if (max < b) max = b;
+        if (max < c) max = c;
+        if (max < d) max = d;
+        return max;
     }
 
 }
