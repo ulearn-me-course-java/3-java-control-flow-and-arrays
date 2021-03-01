49d589d617abb8ca48d68f2ec7e51d2cc7bc444a
DarkLight <max.gubar@outlook.com>
2021-03-01 18:20:57 +0500

    Губарь Максим task6

--- task06/src/com.example.task06/Task06Main.java
+++ task06/src/com.example.task06/Task06Main.java
@@ -10,8 +10,16 @@ public class Task06Main {
     }
 
     static int getMax(int a, int b, int c, int d) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        int[] array = new int[3];
+        array[0] = b;
+        array[1] = c;
+        array[2] = d;
+        int max = a;
+        for (int j : array) {
+            if (j > max)
+                max = j;
+        }
+        return max;
     }
 
 }
