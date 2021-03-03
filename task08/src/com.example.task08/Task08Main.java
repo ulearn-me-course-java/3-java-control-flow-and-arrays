58ce58496b70d0a7fe4b345ee3e36dd149d375eb
Maksim <rsincara@mail.ru>
2021-03-04 01:27:46 +0500

    Задача 3.8 - Ермаков М.

--- task08/src/com.example.task08/Task08Main.java
+++ task08/src/com.example.task08/Task08Main.java
@@ -1,20 +1,17 @@
 package com.example.task08;
 
+import java.util.Arrays;
+
 public class Task08Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        int[] arr = new int[2];
-        arr[0] = 2;
-        arr[1] = 3;
-        System.out.println(mult(arr));
-         */
     }
 
     static long mult(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 1;
+        long mul = arr.length == 0 ? 0 : 1;
+        for (int i : arr) {
+            mul *= i;
+        }
+        return mul;
     }
 
 }
