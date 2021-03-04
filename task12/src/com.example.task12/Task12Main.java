0b6309036a0bfe443077d521e459f71d83c65309
Anna228322 <anna.fittsdzherald@mail.ru>
2021-03-04 15:24:33 +0500

    12

--- task12/src/com.example.task12/Task12Main.java
+++ task12/src/com.example.task12/Task12Main.java
@@ -12,7 +12,15 @@ public class Task12Main {
     }
 
     static void selectionSort(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (arr == null || arr.length == 0)
+            return;
+        for (int i = 0; i < arr.length; i++)
+            for (int j = i; j < arr.length; j++)
+                if (arr[i] >= arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
     }
 
 }
