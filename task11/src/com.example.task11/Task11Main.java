d77fcff7b6dbeeb4ef7fb6e71973835f7fcc020e
Nikita Lyadov (Anchovy) <hakker990@gmail.com>
2021-03-08 22:00:07 +0500

    Fix

--- task11/src/com.example.task11/Task11Main.java
+++ task11/src/com.example.task11/Task11Main.java
@@ -12,6 +12,8 @@ public class Task11Main {
     }
 
     static void swap(int[] arr) {
+        if(arr == null || arr.length == 0) return;
+
         int minIndex = 0;
         int min = arr[0];
         for (int i = 0; i< arr.length; i++) {
