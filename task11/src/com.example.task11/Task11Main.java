87ddc8a8050ae7f21b5bf2ae5d2054c0316583b6
sashapetrova28 <sasha_281200@mail.ru>
2021-03-23 19:44:40 +0500

    Петрова Александра 3.11

--- task11/src/com.example.task11/Task11Main.java
+++ task11/src/com.example.task11/Task11Main.java
@@ -2,17 +2,23 @@ package com.example.task11;
 
 public class Task11Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+
         int[] arr = {7, 5, 9};
         swap(arr);
         System.out.println(java.util.Arrays.toString(arr));
-         */
+
     }
 
     static void swap(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-    }
 
+        if (arr != null) {
+            for (int i = 1; i < arr.length - 1; i++) {
+                if (arr[0] > arr[i]) {
+                    int a = arr[0];
+                    arr[0] = arr[i];
+                    arr[i] = a;
+                }
+            }
+        }
+    }
 }
