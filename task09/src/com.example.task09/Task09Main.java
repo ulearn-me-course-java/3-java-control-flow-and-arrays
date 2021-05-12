661fb034c4bba482f1d8b4ecbf9200288f5c715b
iki.ovo <iki.ovo@mail.ru>
2021-05-12 16:42:42 +0500

    Modina task09

--- task09/src/com.example.task09/Task09Main.java
+++ task09/src/com.example.task09/Task09Main.java
@@ -14,8 +14,11 @@ public class Task09Main {
     }
 
     static int min(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        int i = Integer.MAX_VALUE;
+        for (int num: arr) {
+            i = Math.min(i, num);
+        }
+        return i;
     }
 
 }
