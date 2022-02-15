b064b1b39845acff0dc0b9d22a8898f339c1f6de
Anny <70793585+anyast103@users.noreply.github.com>
2022-02-15 17:31:52 +0300

    Task_3.9

--- task09/src/com.example.task09/Task09Main.java
+++ task09/src/com.example.task09/Task09Main.java
@@ -1,21 +1,22 @@
 package com.example.task09;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.OptionalInt;
+import java.util.stream.*;
+
 public class Task09Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         int[] arr = new int[3];
         arr[0] = 2;
         arr[1] = -1;
         arr[2] = 3;
         System.out.println(min(arr));
-         */
     }
 
     static int min(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        OptionalInt min = Arrays.stream(arr).min();
+        return min.getAsInt();
     }
 
 }
