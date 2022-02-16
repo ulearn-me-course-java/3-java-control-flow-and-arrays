50b49233595997e256531a9951f9273a92326c32
FremyXS <antonovch13@mail.ru>
2022-02-16 12:05:46 +0500

    Задание 3.5 Овчинников Антон ПРИ-202

--- task05/src/com.example.task05/Task05Main.java
+++ task05/src/com.example.task05/Task05Main.java
@@ -10,7 +10,19 @@ public class Task05Main {
     }
 
     static int getMin(int a, int b, int c) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+
+        if(a < b){
+            if(a < c)
+                return a;
+            else
+                return b < c? b : c;
+        }
+        else {
+            if(b < c)
+                return b;
+            else
+                return a < c? a : c;
+        }
+
     }
 }
