e0232827b94715e766c3848e0618e0643823141e
nodlya <nadya-popova-02@mail.ru>
2022-03-02 12:05:32 +0500

    Task 3.10

--- task10/src/com.example.task10/Task10Main.java
+++ task10/src/com.example.task10/Task10Main.java
@@ -1,18 +1,28 @@
 package com.example.task10;
 
+import java.util.Arrays;
+
 public class Task10Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        int[] arr = {7, 5, 9};
+
+        int[] arr = {1, 2, 3, 4};
         System.out.println(numMin(arr));
-         */
+
     }
 
     static int numMin(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        int min = Arrays.stream(arr).min().getAsInt();
+        int minIndex = 0;
+        int minCount = 0;
+        for (int i =0; i<arr.length;i++)
+            if (arr[i] == min)  {
+                minCount++;
+            minIndex = i;
+            }
+
+        return minIndex;
     }
 
 }
