1fa5b127a3cf292d571491045c6fb1db84c2eba0
nodlya <nadya-popova-02@mail.ru>
2022-03-02 11:37:10 +0500

    Task 3.8

--- task08/src/com.example.task08/Task08Main.java
+++ task08/src/com.example.task08/Task08Main.java
@@ -1,5 +1,8 @@
 package com.example.task08;
 
+import java.math.BigInteger;
+import java.util.Arrays;
+
 public class Task08Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
@@ -13,8 +16,10 @@ public class Task08Main {
     }
 
     static long mult(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 1;
+        if (arr.length == 0) return 0;
+       long mult = 1;
+        for (int t : arr) mult *= t;
+        return mult;
     }
 
 }
