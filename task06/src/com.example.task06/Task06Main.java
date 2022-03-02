227a902377b57f39d395f808b680807c3e588222
nodlya <nadya-popova-02@mail.ru>
2022-03-02 10:48:43 +0500

    Task 3.6

--- task06/src/com.example.task06/Task06Main.java
+++ task06/src/com.example.task06/Task06Main.java
@@ -1,17 +1,24 @@
 package com.example.task06;
 
+import javax.swing.text.html.Option;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.OptionalInt;
+
 public class Task06Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         System.out.println(getMax(1, 2, 3, 4));
-         */
+
     }
 
     static int getMax(int a, int b, int c, int d) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        int[] arr = {a,b,c,d};
+        OptionalInt max = Arrays.stream(arr).max();
+        return max.getAsInt();
     }
 
 }
