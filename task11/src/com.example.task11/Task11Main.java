96b55cf69f8189acc42763f70986b5fc6147826c
Saveliy <savelussava@mail.ru>
2022-03-04 14:44:32 +0500

    task11. Кудрявцев Савелий

--- task11/src/com.example.task11/Task11Main.java
+++ task11/src/com.example.task11/Task11Main.java
@@ -12,7 +12,15 @@ public class Task11Main {
     }
 
     static void swap(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if(!(arr == null || arr.length == 0)) {
+            for (int i = 1; i < arr.length; i++) {
+                if (arr[i] <= arr[0]) {
+                    int temp = arr[i];
+                    arr[i] = arr[0];
+                    arr[0] = temp;
+                }
+            }
+        }
     }
 
 }
