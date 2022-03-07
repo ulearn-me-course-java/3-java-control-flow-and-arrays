0571901606823f6310df6c77a47fba5e0839e0ed
Sekou Samassa <71543096+sksamassa@users.noreply.github.com>
2022-03-07 15:52:29 +0500

    Update Task05Main.java

--- task05/src/com.example.task05/Task05Main.java
+++ task05/src/com.example.task05/Task05Main.java
@@ -4,13 +4,21 @@ public class Task05Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+       
         System.out.println(getMin(5,4,3));
-         */
+        
     }
 
     static int getMin(int a, int b, int c) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        int[] nums = {a, b, c};
+        int min = nums[0];
+        for(int i = 0; i < 3; i++)
+        {
+            if(min > nums[i])
+            {
+                min = nums[i];
+            }
+        }
+        return min;
     }
 }
