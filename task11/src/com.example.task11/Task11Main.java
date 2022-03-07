ce521af4a3de2e327aea90a1b7e738cb18164a3a
Sekou Samassa <71543096+sksamassa@users.noreply.github.com>
2022-03-07 16:05:23 +0500

    Update Task11Main.java

--- task11/src/com.example.task11/Task11Main.java
+++ task11/src/com.example.task11/Task11Main.java
@@ -4,15 +4,40 @@ public class Task11Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+       
         int[] arr = {7, 5, 9};
         swap(arr);
         System.out.println(java.util.Arrays.toString(arr));
-         */
+        
     }
 
-    static void swap(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+     static void swap(int[] arr) {
+        for(int ar : arr)
+        {
+            System.out.print(ar + " ");
+        }System.out.println();
+
+        int j = numMin(arr);
+        int temp = arr[0];
+        arr[0] = arr[j];
+        arr[j] = temp;
+
+        for(int ar : arr)
+        {
+            System.out.print(ar + " ");
+        }
+    }
+    static int numMin(int[] arr)
+    {
+        int minNum = arr[0];
+        for(int i = 0; i < arr.length; i++)
+        {
+            if(arr[i] < minNum)
+            {
+                minNum = i;
+            }
+        }
+        return minNum;
     }
 
 }
