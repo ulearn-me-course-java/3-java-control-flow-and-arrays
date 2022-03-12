b49e5bc93c485c16a1038938a554d0996a64d86e
gorosheek <yulyagroshenko@mail.ru>
2022-03-12 13:30:45 +0300

    Task03

--- task03/src/com.example.task03/Tests.java
+++ task03/src/com.example.task03/Tests.java
@@ -7,13 +7,14 @@ public class Tests {
 
     @Test
     public void testGetNameOfWeekDays() {
-        Assert.assertEquals("1 - понедельник", "понедельник", Task03Main.getNameOfWeekDays(1));
-        Assert.assertEquals("2 - вторник", "вторник", Task03Main.getNameOfWeekDays(2));
-        Assert.assertEquals("3 - среда", "среда", Task03Main.getNameOfWeekDays(3));
-        Assert.assertEquals("4 - четверг", "четверг", Task03Main.getNameOfWeekDays(4));
-        Assert.assertEquals("5 - пятница", "пятница", Task03Main.getNameOfWeekDays(5));
-        Assert.assertEquals("6 - суббота", "суббота", Task03Main.getNameOfWeekDays(6));
-        Assert.assertEquals("7 - воскресенье", "воскресенье", Task03Main.getNameOfWeekDays(7));
+        Assert.assertEquals("1 - понедельник", "Monday", Task03Main.getNameOfWeekDays(1));
+        Assert.assertEquals("2 - вторник", "Tuesday", Task03Main.getNameOfWeekDays(2));
+        Assert.assertEquals("3 - среда", "Wednesday", Task03Main.getNameOfWeekDays(3));
+        Assert.assertEquals("4 - четверг", "Thursday", Task03Main.getNameOfWeekDays(4));
+        Assert.assertEquals("5 - пятница", "Friday", Task03Main.getNameOfWeekDays(5));
+        Assert.assertEquals("6 - суббота", "Saturday", Task03Main.getNameOfWeekDays(6));
+        Assert.assertEquals("7 - воскресенье", "Sunday", Task03Main.getNameOfWeekDays(7));
+        Assert.assertEquals("8 - That's not a day of the week", "That's not a day of the week", Task03Main.getNameOfWeekDays(8));
     }
 
 }
