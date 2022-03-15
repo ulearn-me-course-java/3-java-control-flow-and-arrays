6d4602f9f149a27e63c1b1df94ad1240d1a55718
Amirka2 <far20022002@mail.ru>
2022-03-15 22:54:31 +0500

    Задание 12. Файзуллин Амир, ПрИ-201.

--- task12/src/com.example.task12/Task12Main.java
+++ task12/src/com.example.task12/Task12Main.java
@@ -12,7 +12,30 @@ public class Task12Main {
     }
 
     static void selectionSort(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (arr != null){
+            for (int i = 0; i < arr.length; i++){
+                swap(arr, i);
+            }
+        }
     }
+    static void swap(int[] arr, int index) {
+        if (arr != null && arr.length > 0) {
+            int numOfMin = numMin(arr, index);
+            int temp = arr[index];
+            arr[index] = arr[numOfMin];
+            arr[numOfMin] = temp;
+        }
+    }
+    static int numMin(int[] arr, int index) {
+        int numOfMin = index;
+        int minNum = arr[index];
+        for(int i = index; i < arr.length; i++){
+            if (arr[i] < minNum) {
+                minNum = arr[i];
+                numOfMin = i;
+            }
+        }
 
+        return numOfMin;
+    }
 }
