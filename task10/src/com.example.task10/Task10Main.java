63f3f5b16fe6d27acc510e3735ac7499a3a128bd
vlaD <vlad.74022@gmail.com>
2022-03-16 11:39:35 +0500

    Task3.10 Ртый Владислав, ПрИ-201.

--- task10/src/com.example.task10/Task10Main.java
+++ task10/src/com.example.task10/Task10Main.java
@@ -11,8 +11,16 @@ public class Task10Main {
     }
 
     static int numMin(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        int minId = 0;
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] <= min) {
+                min = arr[i];
+                minId = i;
+            }
+
+        }
+        return minId;
     }
 
 }
