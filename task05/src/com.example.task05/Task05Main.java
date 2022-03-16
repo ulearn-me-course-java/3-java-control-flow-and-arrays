0cc741d5a19bfe9883809bf140401fa983661963
MindHardt <71509337+MindHardt@users.noreply.github.com>
2022-03-16 10:25:36 +0500

    Бабин Task05

--- task05/src/com.example.task05/Task05Main.java
+++ task05/src/com.example.task05/Task05Main.java
@@ -11,6 +11,8 @@ public class Task05Main {
 
     static int getMin(int a, int b, int c) {
         //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        if (a < b && a < c) return a;
+        return b < c ? b : c;
+
     }
 }
