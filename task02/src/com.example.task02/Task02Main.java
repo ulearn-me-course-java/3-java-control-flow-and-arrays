291939cb0da9223e79924ca2b5c0bb0a7a1f87bb
stbmte <ateruhin388@gmail.com>
2022-06-01 06:24:09 +0300

    task2

--- task02/src/com.example.task02/Task02Main.java
+++ task02/src/com.example.task02/Task02Main.java
@@ -10,6 +10,18 @@ public class Task02Main {
     }
 
     static String getSeason(int monthNumber) {
-        return "";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if(monthNumber <= 2 || monthNumber == 12){
+            return "зима";
+        }
+        if(monthNumber > 2 && monthNumber <= 5){
+            return "весна";
+        }
+        if(monthNumber > 5 && monthNumber <= 8){
+            return "лето";
+        }
+        if(monthNumber > 8 && monthNumber <= 11){
+            return "осень";
+        }
+        return "Введен неправильный месяц";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
     }
 }
