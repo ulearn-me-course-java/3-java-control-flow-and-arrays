5470e107a4714893adf4053de07588171a883272
Arseny <20mineslend14@gmail.com>
2022-09-17 23:46:38 +0500

    task13

--- task13/src/com.example.task13/Task13Main.java
+++ task13/src/com.example.task13/Task13Main.java
@@ -12,8 +12,28 @@ public class Task13Main {
     }
 
     static int[] removeMoreThen1000(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return  null;
+        if(arr != null && arr.length  != 0)
+        {
+            int size = 0;
+
+            for (int j : arr) {
+                if (j < 1000)
+                    size++;
+            }
+
+            int[] newArr = new int[size];
+
+            int fill = 0;
+            for (int j : arr) {
+                if (j < 1000) {
+                    newArr[fill] = j;
+                    fill++;
+                }
+            }
+
+            return  newArr;
+        }
+        return arr;
     }
 
 }
