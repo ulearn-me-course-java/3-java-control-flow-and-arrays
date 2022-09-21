b61600126930164a99e4ddb4bdefb79d8e72c61b
Egorov Mikhail <m-5321e@yandex.com>
2022-09-22 00:37:00 +0500

    Егоров Михаил. task10

--- task10/src/com.example.task10/Task10Main.java
+++ task10/src/com.example.task10/Task10Main.java
@@ -11,8 +11,14 @@ public class Task10Main {
     }
 
     static int numMin(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        int indexMin = 0;
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length; i++)
+            if (arr[i] <= min) {
+                indexMin = i;
+                min = arr[i];
+            }
+        return indexMin;
     }
 
 }
