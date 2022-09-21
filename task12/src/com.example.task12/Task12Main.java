10789ae6b6dca490c4ed8e9412660a41a5f70fb7
Egorov Mikhail <m-5321e@yandex.com>
2022-09-22 01:04:59 +0500

    Егоров Михаил. task12

--- task12/src/com.example.task12/Task12Main.java
+++ task12/src/com.example.task12/Task12Main.java
@@ -12,7 +12,20 @@ public class Task12Main {
     }
 
     static void selectionSort(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (arr != null && arr.length > 0) {
+            for (int i = 0; i < arr.length; i++) {
+                int indexMin = 0;
+                int min = Integer.MAX_VALUE;
+                for (int j = i; j < arr.length; j++) {
+                    if (arr[j] < min) {
+                        indexMin = j;
+                        min = arr[j];
+                    }
+                }
+                arr[indexMin] = arr[i];
+                arr[i] = min;
+            }
+        }
     }
 
 }
