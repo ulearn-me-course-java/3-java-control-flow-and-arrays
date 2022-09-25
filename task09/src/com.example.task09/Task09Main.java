836404e0886eb149c0a745e0887268d3964286f0
Your Soul <timurnigmatulin6@gmail.com>
2022-09-25 17:18:13 +0500

    task09

--- task09/src/com.example.task09/Task09Main.java
+++ task09/src/com.example.task09/Task09Main.java
@@ -14,8 +14,11 @@ public class Task09Main {
     }
 
     static int min(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        int min = arr[0];
+        for (int elem : arr){
+            if (elem < min) min = elem;
+        }
+        return min;
     }
 
 }
