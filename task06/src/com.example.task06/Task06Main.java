e302ab9527550fd41b6ed3464f29d6fe996603d5
Alina <wbrflf3301@outlook.com>
2022-09-25 21:18:58 +0500

    Выполнено задание 6

--- task06/src/com.example.task06/Task06Main.java
+++ task06/src/com.example.task06/Task06Main.java
@@ -1,5 +1,4 @@
 package com.example.task06;
-
 public class Task06Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
@@ -11,7 +10,6 @@ public class Task06Main {
 
     static int getMax(int a, int b, int c, int d) {
         //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        return Math.max(Math.max(a, b), Math.max(c, d));
     }
-
 }
