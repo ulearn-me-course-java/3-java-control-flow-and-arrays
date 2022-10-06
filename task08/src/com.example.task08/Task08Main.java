65eb577e0091e6866c7a8bcce498e761821dd463
Vilena <vilena.saitgalina@mail.ru>
2022-10-06 18:00:32 +0500

    Саитгалина Вилена задание 8

--- task08/src/com.example.task08/Task08Main.java
+++ task08/src/com.example.task08/Task08Main.java
@@ -13,8 +13,14 @@ public class Task08Main {
     }
 
     static long mult(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 1;
+        long mul = 1;
+        if (arr.length == 0) {
+            return 0;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            mul *= arr[i];
+        }
+        return mul;
     }
 
 }
