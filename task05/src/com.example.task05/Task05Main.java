55dce25b30c4878a47b73a54151e197953c56dcf
Vilena <vilena.saitgalina@mail.ru>
2022-10-06 11:48:24 +0500

    Саитгалина Вилена задание 5

--- task05/src/com.example.task05/Task05Main.java
+++ task05/src/com.example.task05/Task05Main.java
@@ -10,7 +10,13 @@ public class Task05Main {
     }
 
     static int getMin(int a, int b, int c) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        int min = Integer.MAX_VALUE;
+        if (a < min)
+            min = a;
+        if (b < min)
+            min = b;
+        if (c < min)
+            min = c;
+        return min;
     }
 }
