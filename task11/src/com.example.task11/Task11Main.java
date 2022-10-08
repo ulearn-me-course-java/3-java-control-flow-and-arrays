3a8d34df14ae4a20980064bc09a87fc23591e3ba
student <rustamdvl@yandex.ru>
2022-10-08 15:48:33 +0500

    Давлетханов Рустам. Задание 11

--- task11/src/com.example.task11/Task11Main.java
+++ task11/src/com.example.task11/Task11Main.java
@@ -1,18 +1,28 @@
 package com.example.task11;
 
+import org.graalvm.compiler.lir.Variable;
+
 public class Task11Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
         int[] arr = {7, 5, 9};
         swap(arr);
         System.out.println(java.util.Arrays.toString(arr));
-         */
     }
 
     static void swap(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        int min = arr[0];
+        for (int j : arr) min = Math.min(min, j);
+        int index = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == min)
+                index = i;
+        }
+        int temp = arr[0];
+        arr[0] = arr[index];
+        arr[index] = temp;
     }
 
 }
