84ab452668d74be7f4f61c7b9ca750db188f34f5
nbespalovv <Dnevnik1400@mail.ru>
2022-10-09 23:23:08 +0500

    Задание 11.

--- task11/src/com.example.task11/Task11Main.java
+++ task11/src/com.example.task11/Task11Main.java
@@ -12,6 +12,21 @@ public class Task11Main {
     }
 
     static void swap(int[] arr) {
+
+        if(arr != null && arr.length != 0) {
+            int min = Integer.MAX_VALUE;
+            int index = -1;
+            int temp = 0;
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] < min) {
+                    min = arr[i];
+                    index = i;
+                }
+            }
+            temp = arr[0];
+            arr[0] = arr[index];
+            arr[index] = temp;
+        }
         //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
     }
 
