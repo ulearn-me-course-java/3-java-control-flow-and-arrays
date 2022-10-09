78d6a68388cc7aa533df6b5167ebcae2508bd0a6
Архипов Евгений <arkhipov.zena@gmail.com>
2022-10-09 22:07:36 +0500

    Hi, I am Gregg

--- task10/src/com.example.task10/Task10Main.java
+++ task10/src/com.example.task10/Task10Main.java
@@ -4,15 +4,24 @@ public class Task10Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         int[] arr = {7, 5, 9};
         System.out.println(numMin(arr));
-         */
+
     }
 
     static int numMin(int[] arr) {
+        int min = Integer.MAX_VALUE;
+        int count = 0;
+        for (int i = 0; i < arr.length; i++)
+            if (min >= arr[i]) {
+                min = arr[i];
+                count = i;
+            }
+
+
         //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        return count;
     }
 
 }
--- task13/src/com.example.task13/Task13Main.java
+++ task13/src/com.example.task13/Task13Main.java
@@ -4,16 +4,30 @@ public class Task13Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         int[] arr = {9, 1100, 7, 8};
         removeMoreThen1000(arr);
         System.out.println(java.util.Arrays.toString(arr));
-         */
+
     }
 
     static int[] removeMoreThen1000(int[] arr) {
+        if (arr == null || arr.length == 0)
+            return arr;
+        int countBig = 0;
+        for (int i = 0; i < arr.length; i++)
+            if (arr[i] > 1000)
+                countBig++;
+        int[] ans = new int[arr.length - countBig];
+        int i1 = 0;
+        for (int i = 0; i < arr.length; i++)
+            if (arr[i] <= 100) {
+                ans[i1] = arr[i];
+                i1++;
+            }
+        arr = ans;
         //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return  null;
+        return arr;
     }
 
 }
