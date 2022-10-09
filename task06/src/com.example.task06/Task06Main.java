eaca4421a4a2a4d0cd9a744fb79b656ed1f7d7f7
denis <kuchukovdenis2003@mail.ru>
2022-10-09 19:42:44 +0500

    Кучуков Денислам. Модуль 3

--- task06/src/com.example.task06/Task06Main.java
+++ task06/src/com.example.task06/Task06Main.java
@@ -2,16 +2,16 @@ package com.example.task06;
 
 public class Task06Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        System.out.println(getMax(1, 2, 3, 4));
-         */
+
     }
 
     static int getMax(int a, int b, int c, int d) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        int[] arr = {a, b, c, d};
+        int max = arr[0];
+        for (int i = 1 ; i < arr.length ; i++) {
+            max = Math.max(max, arr[i]);
+        }
+        return max;
     }
 
 }
