7af48b5a2d8cb23ed68c6e27cc9bb6818d7a6032
nbespalovv <Dnevnik1400@mail.ru>
2022-10-09 22:57:32 +0500

    Задание 9.

--- task09/src/com.example.task09/Task09Main.java
+++ task09/src/com.example.task09/Task09Main.java
@@ -14,8 +14,14 @@ public class Task09Main {
     }
 
     static int min(int[] arr) {
+
+        int min = Integer.MAX_VALUE;
+        for(int i =0; i < arr.length; i++){
+            if (arr[i] < min) min = arr[i];
+        }
+        return min;
         //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+
     }
 
 }
