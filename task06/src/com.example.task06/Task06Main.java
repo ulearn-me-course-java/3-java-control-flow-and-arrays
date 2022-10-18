97502a0ed346adf6722230460b7c0435a00c5573
Boris Tarelkin <tarelkinba@gmail.com>
2022-10-18 22:12:32 +0500

    Горбунов Егор ПрИ-201

--- task06/src/com.example.task06/Task06Main.java
+++ task06/src/com.example.task06/Task06Main.java
@@ -4,14 +4,13 @@ public class Task06Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        System.out.println(getMax(1, 2, 3, 4));
-         */
+
+        System.out.println(getMax(167, 82, 33, 47));
     }
 
     static int getMax(int a, int b, int c, int d) {
         //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        return Math.max(Math.max(a, b), Math.max(c, d));
     }
 
 }
