bac6bcc39c4ab34a1b545de0a0c9c3c4bba73c16
Olga <dikay.pech@mail.ru>
2022-10-23 23:44:34 +0300

    Мотовилова Ольга ПрИ-201 Модуль 3

--- task02/src/com.example.task02/Task02Main.java
+++ task02/src/com.example.task02/Task02Main.java
@@ -4,12 +4,22 @@ public class Task02Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         System.out.println(getSeason(12));
-         */
+
     }
 
     static String getSeason(int monthNumber) {
-        return "";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (monthNumber < 1 || monthNumber > 12)
+            return "Неверное число";
+        if (monthNumber >= 3 && monthNumber <= 5)
+            return "весна";
+        if (monthNumber > 5 && monthNumber <= 8)
+            return  "лето";
+        if (monthNumber > 8 && monthNumber <= 11)
+            return "осень";
+        else
+            return "зима";
+        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
     }
 }
--- task05/src/com.example.task05/Task05Main.java
+++ task05/src/com.example.task05/Task05Main.java
@@ -4,13 +4,20 @@ public class Task05Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         System.out.println(getMin(5,4,3));
-         */
+
     }
 
     static int getMin(int a, int b, int c) {
         //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        /*
+        if (a <= b && a <= c)
+            return a;
+        if (b <= a && b <= c)
+            return b;
+
+         */
+        return Math.min(Math.min(a, b), c);
     }
 }
