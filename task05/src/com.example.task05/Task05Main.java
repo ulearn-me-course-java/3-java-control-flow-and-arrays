027a91236f5e45685f9a6eaf6281498de4566708
Nastya <Nusty11102003@gmail.com>
2022-11-08 22:28:26 +0500

    Доделала

--- task05/src/com.example.task05/Task05Main.java
+++ task05/src/com.example.task05/Task05Main.java
@@ -4,13 +4,15 @@ public class Task05Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         System.out.println(getMin(5,4,3));
-         */
+
     }
 
     static int getMin(int a, int b, int c) {
         //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        if (b < a) a = b;
+        if (c < a) a = c;
+        return a;
     }
 }
