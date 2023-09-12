c8c5a05311ab17b6efeb4c38f52946adbf9d2dde
informant <artemiy.solodkin@mail.ru>
2023-09-12 21:26:08 +0500

    All Done

--- task12/src/com.example.task12/Task12Main.java
+++ task12/src/com.example.task12/Task12Main.java
@@ -4,15 +4,30 @@ public class Task12Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         int[] arr = {9, 11, 7, 8};
         selectionSort(arr);
         System.out.println(java.util.Arrays.toString(arr));
-         */
+
     }
 
     static void selectionSort(int[] arr) {
         //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-    }
 
+        if (arr == null){
+            return;
+        }
+
+        for (int c = 0; c < arr.length; c++) {
+            int res = c;
+            for (int i = res; i < arr.length; i++) {
+                if (arr[res] >= arr[i]) {
+                    res = i;
+                }
+            }
+            int a = arr[c];
+            arr[c] = arr[res];
+            arr[res] = a;
+        }
+    }
 }
--- task13/src/com.example.task13/Task13Main.java
+++ task13/src/com.example.task13/Task13Main.java
@@ -4,16 +4,35 @@ public class Task13Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        int[] arr = {9, 1100, 7, 8};
-        removeMoreThen1000(arr);
-        System.out.println(java.util.Arrays.toString(arr));
-         */
+
+        int[] arr = null;
+        int[] result = removeMoreThen1000(arr);
+        System.out.println(java.util.Arrays.toString(result));
+
     }
 
     static int[] removeMoreThen1000(int[] arr) {
         //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return  null;
+        if (arr == null){
+            return null;
+        }
+
+        int len = 0;
+        for (int i=0; i< arr.length;i++){
+            if (arr[i]<=1000){
+                len++;
+            }
+        }
+
+        int[] res = new int[len];
+        int ind = 0;
+        for (int i=0; i< arr.length;i++){
+            if (arr[i]<=1000){
+                res[ind]= arr[i];
+                ind++;
+            }
+        }
+        return  res;
     }
 
 }
