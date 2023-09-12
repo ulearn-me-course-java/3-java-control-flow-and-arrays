0efc33d05946057ee423a6d67c59aed4a5aee8de
informant <artemiy.solodkin@mail.ru>
2023-09-12 21:04:13 +0500

    10 task done

--- task10/src/com.example.task10/Task10Main.java
+++ task10/src/com.example.task10/Task10Main.java
@@ -4,15 +4,18 @@ public class Task10Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        int[] arr = {7, 5, 9};
+
+        int[] arr = {2147483647};
         System.out.println(numMin(arr));
-         */
     }
 
     static int numMin(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        int res = 0;
+        for (int i=0;i<arr.length;i++){
+            if (arr[res]>=arr[i]){
+                res = i;
+            }
+        }
+        return res;
     }
-
 }
