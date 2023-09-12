4181860747677034de54941001bc603121c13fbf
Michael Gallinago <michael.gallinago@gmail.com>
2023-09-12 21:35:26 +0500

    Lab3

--- task12/src/com.example.task12/Task12Main.java
+++ task12/src/com.example.task12/Task12Main.java
@@ -2,17 +2,33 @@ package com.example.task12;
 
 public class Task12Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        int[] arr = {9, 11, 7, 8};
+        int[] arr = {5, 5, 5};
         selectionSort(arr);
         System.out.println(java.util.Arrays.toString(arr));
-         */
     }
 
     static void selectionSort(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (arr == null || arr.length == 0)
+        {
+            return;
+        }
+
+        for (int i = 0; i < arr.length; i++)
+        {
+            int minValue = Integer.MAX_VALUE;
+            int index = i;
+            for (int j = i; j < arr.length; j++)
+            {
+                if (minValue >= arr[j]) {
+                    minValue = arr[j];
+                    index = j;
+                }
+            }
+
+            int tempValue = arr[i];
+            arr[i] = arr[index];
+            arr[index] = tempValue;
+        }
     }
 
 }
