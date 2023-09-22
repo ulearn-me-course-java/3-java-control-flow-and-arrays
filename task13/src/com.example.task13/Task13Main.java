40003d20d11309b82af34a27aa2895dd04901edc
Yana Gnedkova <ygnedkova@gmail.com>
2023-09-23 00:09:38 +0500

    Гнедкова Яна Модуль 03

--- task13/src/com.example.task13/Task13Main.java
+++ task13/src/com.example.task13/Task13Main.java
@@ -4,16 +4,31 @@ public class Task13Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         int[] arr = {9, 1100, 7, 8};
         removeMoreThen1000(arr);
         System.out.println(java.util.Arrays.toString(arr));
-         */
-    }
 
+    }
     static int[] removeMoreThen1000(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return  null;
-    }
 
+        if (arr == null) {
+            return null;
+        } else if (arr.length == 0) {
+            return new int[0];
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i : arr) {
+            if (i > 1000) {
+                continue;
+            }
+            sb.append(i).append(" ");
+        }
+        String[] str = sb.toString().split(" ");
+        int[] results = new int[str.length];
+        for (int i = 0; i < results.length; i++) {
+            results[i] = Integer.parseInt(str[i]);
+        }
+        return results;
+    }
 }
