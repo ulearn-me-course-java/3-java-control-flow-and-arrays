4acc605e4d0dfe5911a9af5cdcdeb790a5a867ac
FazylovV <vladislav.fazylov04@gmail.com>
2023-09-24 21:10:21 +0500

    task11 complete

--- task11/src/com.example.task11/Task11Main.java
+++ task11/src/com.example.task11/Task11Main.java
@@ -12,7 +12,20 @@ public class Task11Main {
     }
 
     static void swap(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        int min = Integer.MAX_VALUE;
+        int index = 0;
+        if(arr == null) return;
+        if(arr.length == 0) return;
+
+        for (int i = 0; i < arr.length; i++){
+            if(arr[i] <= min) {
+                min = arr[i];
+                index = i;
+            }
+        }
+
+        arr[index] = arr[0];
+        arr[0] = min;
     }
 
 }
