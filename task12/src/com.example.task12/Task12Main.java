e427f6d2b114df97f893e016742e04e78247547f
Diana <lici4qweru090@gmail.com>
2023-09-25 02:07:09 +0500

    task12 is done

--- task12/src/com.example.task12/Task12Main.java
+++ task12/src/com.example.task12/Task12Main.java
@@ -2,17 +2,25 @@ package com.example.task12;
 
 public class Task12Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        int[] arr = {9, 11, 7, 8};
+        int[] arr = {8, -10, 5, -3, 1000, 0};
         selectionSort(arr);
         System.out.println(java.util.Arrays.toString(arr));
-         */
     }
 
     static void selectionSort(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        if (arr == null){
+            return;
+        }
+        for (int k = 0; k < arr.length; k++) {
+            int min = k;
+            for (int i = k; i < arr.length; i++) {
+                if (arr[min] >= arr[i]) {
+                    min = i;
+                }
+            }
+            int interim = arr[k];
+            arr[k] = arr[min];
+            arr[min] = interim;
+        }
     }
-
 }
