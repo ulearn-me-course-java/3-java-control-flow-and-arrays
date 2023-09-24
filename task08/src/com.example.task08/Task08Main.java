e24b15c4a55e4177958910bd7eff11eade131c07
vadim471 <tissen.vadimka@mail.ru>
2023-09-25 05:22:36 +0700

    Тиссен Вадим ПрИ-202

--- task08/src/com.example.task08/Task08Main.java
+++ task08/src/com.example.task08/Task08Main.java
@@ -2,19 +2,23 @@ package com.example.task08;
 
 public class Task08Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        int[] arr = new int[2];
-        arr[0] = 2;
-        arr[1] = 3;
+        int[] arr = new int[0];
+        //arr[0] = 0;
+        //arr[1] = 3;
         System.out.println(mult(arr));
-         */
     }
 
     static long mult(int[] arr) {
         //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 1;
+        long multip = 1;
+        if (arr.length == 0)
+            System.out.println("произведение пустого массива = 0");
+        else {
+            for (int i = 0; i < arr.length; i++) {
+                multip *= arr[i];
+            }
+        }
+        return multip;
     }
 
 }
--- task12/src/com.example.task12/Task12Main.java
+++ task12/src/com.example.task12/Task12Main.java
@@ -2,16 +2,28 @@ package com.example.task12;
 
 public class Task12Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
         int[] arr = {9, 11, 7, 8};
         selectionSort(arr);
         System.out.println(java.util.Arrays.toString(arr));
-         */
     }
 
     static void selectionSort(int[] arr) {
+        try {
+            int minum;
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = 0; j < arr.length; j++) {
+                    if (arr[i] < arr[j]) {
+                        minum = arr[j];
+                        arr[j] = arr[i];
+                        arr[i] = minum;
+                    }
+                }
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("если пришедший массив пуст, то ничего делать не нужно");
+        } catch (NullPointerException e) {
+            System.out.println("если пришедший массив null, то ничего делать не нужно");
+        }
         //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
     }
 
