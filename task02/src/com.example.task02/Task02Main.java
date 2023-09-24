9906a1a7a1d83dc94311ab265dc449a5bd1717d9
Diana <lici4qweru090@gmail.com>
2023-09-25 01:21:05 +0500

    task02 is done

--- task02/src/com.example.task02/Task02Main.java
+++ task02/src/com.example.task02/Task02Main.java
@@ -2,14 +2,18 @@ package com.example.task02;
 
 public class Task02Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        System.out.println(getSeason(12));
-         */
+        System.out.println(getSeason(7));
     }
 
     static String getSeason(int monthNumber) {
-        return "";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        String result = "зима";
+        if (monthNumber >= 3 && monthNumber<=5){
+            result = "весна";
+        }else if (monthNumber >= 6 && monthNumber <=8 ){
+            result = "лето";
+        } else if (monthNumber >= 9 && monthNumber <= 11){
+            result = "осень";
+        }
+        return result;
     }
 }
