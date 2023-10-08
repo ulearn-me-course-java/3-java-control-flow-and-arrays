bac3649b729f5eef10800a4c54cdfd8279db9e4b
Anatoliy <magic-t@bk.ru>
2023-10-09 00:14:39 +0500

    Task13 Головин А.В

--- task13/src/com.example.task13/Task13Main.java
+++ task13/src/com.example.task13/Task13Main.java
@@ -2,18 +2,35 @@ package com.example.task13;
 
 public class Task13Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+
         int[] arr = {9, 1100, 7, 8};
         removeMoreThen1000(arr);
         System.out.println(java.util.Arrays.toString(arr));
-         */
+
     }
 
     static int[] removeMoreThen1000(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return  null;
+        if (arr == null || arr.length == 0){
+            return arr;
+        }
+
+        int finalLenght = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] <= 1000) {
+                finalLenght++;
+            }
+        }
+
+        int[] res = new int[finalLenght];
+        int j = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] <= 1000) {
+                res[j] = arr[i];
+                j++;
+            }
+        }
+
+        return res;
     }
 
 }
