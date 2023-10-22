bc76d5153528d0374227dba8a604967bd422fcd3
LoiLok <artemklpkv@gmail.com>
2023-10-23 03:53:44 +0500

    task07/2

--- task07/src/com.example.task07/Task07Main.java
+++ task07/src/com.example.task07/Task07Main.java
@@ -12,9 +12,8 @@ public class Task07Main {
 
     static int sum(int[] arr) {
         int sum = 0;
-        for(int i = 0; i < arr.length;i++)
-        {
-            sum += arr[i];
+        for (int j : arr) {
+            sum += j;
         }
         return sum;
     }
