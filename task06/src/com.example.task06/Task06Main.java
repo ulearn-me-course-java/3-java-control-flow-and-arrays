a9e806335609ce14b844626e77f42cfb8116370c
Kastt2705 <zhenya.litovchenko.2000@mail.ru>
2023-10-22 21:46:13 +0500

    Litovchenko E.G. Pri-201

--- task06/src/com.example.task06/Task06Main.java
+++ task06/src/com.example.task06/Task06Main.java
@@ -1,17 +1,26 @@
 package com.example.task06;
 
 public class Task06Main {
-    public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        System.out.println(getMax(1, 2, 3, 4));
-         */
+    public static void main(String[] args)
+    {
+        System.out.println(getMax(228, 27, 12, 55));
     }
 
-    static int getMax(int a, int b, int c, int d) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+    static int getMax(int a, int b, int c, int d)
+    {
+        if (a > b && a > c && a > d)
+        {
+            return a;
+        }
+        else if (b > a && b > c && b > d)
+        {
+            return b;
+        }
+        else if (c > a && c > b && c > d)
+        {
+            return c;
+        }
+        return d;
     }
 
 }
