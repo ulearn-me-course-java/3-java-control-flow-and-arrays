c2e6e948d3da0eda12897b59a9e7ae2de79f9340
Kango911 <lipenkov.a61@gmail.com>
2023-10-23 10:18:33 +0500

    reda ts13

--- task13/src/com.example.task13/Task13Main.java
+++ task13/src/com.example.task13/Task13Main.java
@@ -3,28 +3,31 @@ package com.example.task13;
 public class Task13Main {
     public static void main(String[] args) {
         int[] arr = {9, 1100, 7, 8};
-        removeMoreThen1000(arr);
+        arr = removeMoreThen1000(arr);
         System.out.println(java.util.Arrays.toString(arr));
     }
 
+
     static int[] removeMoreThen1000(int[] arr) {
-        if (arr == null) {
+        if (arr == null){
             return null;
-        } else if (arr.length == 0) {
-            return new int[0];
         }
-        StringBuilder sb = new StringBuilder();
+
+        int count = 0;
         for (int i : arr) {
-            if (i > 1000) {
-                continue;
+            if (i <= 1000) {
+                count++;
             }
-            sb.append(i).append(" ");
         }
-        String[] resStrings = sb.toString().split(" ");
-        int[] res = new int[resStrings.length];
-        for (int i = 0; i < res.length; i++) {
-            res[i] = Integer.parseInt(resStrings[i]);
+
+        int[] resultArr = new int[count];
+        int index = 0;
+        for (int j : arr) {
+            if (j <= 1000) {
+                resultArr[index] = j;
+                index++;
+            }
         }
-        return res;
+        return resultArr;
     }
 }
