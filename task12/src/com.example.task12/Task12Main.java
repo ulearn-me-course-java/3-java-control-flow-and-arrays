3a5ca0163680d5f8726a0047415cc6a316e1d3b1
MabusTFC <113986000+MabusTFC@users.noreply.github.com>
2023-11-19 23:32:07 +0500

    Смирнов И.А ПрИ-101 Лаб 3

--- task12/src/com.example.task12/Task12Main.java
+++ task12/src/com.example.task12/Task12Main.java
@@ -1,18 +1,32 @@
 package com.example.task12;
-
 public class Task12Main {
     public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        int[] arr = {9, 11, 7, 8};
+
+        int[] arr = {9, 11, 7, 8,98,1};
         selectionSort(arr);
         System.out.println(java.util.Arrays.toString(arr));
-         */
+
     }
 
     static void selectionSort(int[] arr) {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+
+        if (arr == null || arr.length == 0){
+            return;
+        }
+
+        for (int i = 0; i < arr.length; ++i){
+            int min = Integer.MAX_VALUE;
+            int minElementNumber = 0;
+            for (int j = i; j < arr.length; ++j){
+                if(min > arr[j]){
+                    min = arr[j];
+                    minElementNumber = j;
+                }
+            }
+            int intermediate = arr[i];
+            arr[i] = min;
+            arr[minElementNumber] = intermediate;
+        }
     }
 
 }
