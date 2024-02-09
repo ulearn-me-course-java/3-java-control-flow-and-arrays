1471c5066228c7eecc982fd82ecd87487d581bea
lenadekart <decartfamily@vk.com>
2024-02-09 23:31:28 +0500

    При помощи структуры HashMap метод getSeason возвращает название сезона взависимости от номера месяца.

--- task02/src/com.example.task02/Task02Main.java
+++ task02/src/com.example.task02/Task02Main.java
@@ -1,5 +1,6 @@
 package com.example.task02;
-
+import java.util.HashMap;
+import java.util.Map;
 public class Task02Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
@@ -10,6 +11,26 @@ public class Task02Main {
     }
 
     static String getSeason(int monthNumber) {
-        return "";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        Map<String, int[]> dictionary = new HashMap<>();
+        dictionary.put("зима", new int[] {12, 1, 2});
+        dictionary.put("весна", new int[] {3, 4, 5});
+        dictionary.put("лето", new int[] {6, 7, 8});
+        dictionary.put("осень", new int[] {9, 10,11} );
+
+        for (String key : dictionary.keySet()){
+            if (inArray(dictionary.get((key)), monthNumber)){
+                return key;
+            }
+        }
+        throw new IllegalArgumentException("month number must be from 1 to 12");
+    }
+
+    public static boolean inArray(int[] array, int toFind) {
+        for (int element : array) {
+            if (element == toFind) {
+                return true;
+            }
+        }
+        return false;
     }
 }
