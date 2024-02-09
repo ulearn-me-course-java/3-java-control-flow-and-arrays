b4fd29b2180575e1d73bc88bbbbeb0669ec3375c
lenadekart <decartfamily@vk.com>
2024-02-09 23:37:36 +0500

    При помощи массива weekNames метод getNameOfWeekDays возаращает название дня недели, взависимости от weekDaysNumber

--- task03/src/com.example.task03/Task03Main.java
+++ task03/src/com.example.task03/Task03Main.java
@@ -10,6 +10,11 @@ public class Task03Main {
     }
 
     static String getNameOfWeekDays(int weekDaysNumber) {
-        return "";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        String[] weekNames = {"понедельник", "вторник", "среда", "четверг", "пятница",
+                "суббота", "воскресенье"};
+        if (weekDaysNumber >= 1 && weekDaysNumber <= 7){
+            return weekNames[weekDaysNumber - 1];
+        }
+        throw new IllegalArgumentException("weekDaysNumber must be from 1 to 7");
     }
 }
